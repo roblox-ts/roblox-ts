9604267da6ccbb460fe65d5b7a8508ccd7f05ceb
LouieK22 <LouieK22@users.noreply.github.com>
2018-11-02 19:11:39 -0500

    Enum tests

---
+++ tests/src/enum.ts
@@ -0,0 +1,20 @@
+// Basic enum test
+export enum Fruits {
+	Apple,
+	Orange,
+	Pear,
+}
+
+// Numeric enum test
+export enum Breads {
+	White = 5,
+	Wheat,
+	Pumpernickel,
+}
+
+// String enums
+export enum Soups {
+	Tomato = "TOMATO",
+	ChickenNoodle = "CHICKENNOODLE",
+	Dumpling = "DUMPLINGG",
+}
