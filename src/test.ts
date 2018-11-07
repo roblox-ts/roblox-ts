634ef3c23e69fa46823cf36fa56626afc1548483
Osyris <osyrisrblx@gmail.com>
2018-11-07 06:05:10 -0500

    improve tests

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@ import { Compiler } from "./class/Compiler";
 require("mocha");
 
 describe("compile", () => {
-	it("should compile all test cases", done => {
+	it("should compile all test cases", async () => {
 		const args = {
 			includePath: "include",
 			modulesPath: "modules",
@@ -12,9 +12,9 @@ describe("compile", () => {
 			noStrict: false,
 		};
 		const compiler = new Compiler("tests/tsconfig.json", args);
-		compiler
-			.compileAll(true)
-			.catch(reason => done(reason))
-			.then(() => done());
+		await compiler.compileAll(true);
+		if (process.exitCode !== 0) {
+			throw new Error("exit code non-zero!");
+		}
 	});
 });
