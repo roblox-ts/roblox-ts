f8c0b260e2252805239c754722b0169562dc4550
Osyris <osyrisrblx@gmail.com>
2018-11-07 16:25:07 -0500

    fix base_types test

--- tests/src/basic_types.spec.ts
+++ tests/src/basic_types.spec.ts
@@ -1,15 +1,22 @@
 export = () => {
-	FOCUS();
 	it("should properly add numbers", () => {
 		expect(1 + 1).to.equal(2);
+		const a = 1;
+		const b = 1;
+		expect(a + b).to.equal(2);
 	});
 
 	it("should properly add strings", () => {
 		expect("a" + "b").to.equal("ab");
+		const a = "a";
+		const b = "b";
+		expect(a + b).to.equal("ab");
 	});
 
 	it("should properly add numbers and strings", () => {
-		expect("1" + 1).to.equal("11");
-		expect(1 + "1").to.equal("11");
+		const a = "2";
+		const b = 1;
+		expect(a + b).to.equal("21");
+		expect(b + a).to.equal("12");
 	});
 };
