be38b8f432aeb705afc48a8f7db4b9e9b9a10275
Osyris <osyrisrblx@gmail.com>
2018-11-08 12:50:50 -0500

    add if.spec.ts

---
+++ tests/src/if.spec.ts
@@ -0,0 +1,16 @@
+export = () => {
+	it("should allow if statements", () => {
+		function foo(n: number) {
+			if (n < 5) {
+				return "bar";
+			} else if (n < 10) {
+				return "foo";
+			} else {
+				return "baz";
+			}
+		}
+		expect(foo(9)).to.equal("foo");
+		expect(foo(4)).to.equal("bar");
+		expect(foo(11)).to.equal("baz");
+	});
+};
