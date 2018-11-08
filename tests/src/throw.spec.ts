680f51bf8b2ae53024ff0508d26e8dd17cba4ac7
LouieK22 <LouieK22@users.noreply.github.com>
2018-11-07 23:09:01 -0600

    Osyris is a picky man

--- tests/src/throw.spec.ts
+++ tests/src/throw.spec.ts
@@ -1,9 +1,9 @@
 export = () => {
-	/* tslint:disable */
 	function doError(): void {
+		/* tslint:disable */
 		throw "this function sucks";
+		/* tslint:enable */
 	}
-	/* tslint:enable */
 
 	it("should convert throw statements", () => {
 		expect(doError).to.throw();
