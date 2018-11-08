aea2ae2304583de3fc556b1f3409594c0ada6a6a
Osyris <osyrisrblx@gmail.com>
2018-11-07 19:14:16 -0500

    add switch.spec.ts

---
+++ tests/src/switch.spec.ts
@@ -0,0 +1,26 @@
+function foo(s: string) {
+	switch (s) {
+		case "a":
+		case "b":
+			return 1;
+		case "c":
+			return 2;
+		case "d":
+			if (true) {
+				break;
+			}
+		default:
+			return -1;
+	}
+	return 3;
+}
+
+export = () => {
+	it("should allow switch statements", () => {
+		expect(foo("a")).to.equal(1);
+		expect(foo("b")).to.equal(1);
+		expect(foo("c")).to.equal(2);
+		expect(foo("d")).to.equal(3);
+		expect(foo("e")).to.equal(-1);
+	});
+};
