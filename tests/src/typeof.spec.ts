65922dcc4ccc32726817c215edd72e596f2aec1f
Osyris <osyrisrblx@gmail.com>
2018-11-08 02:32:56 -0500

    typeof.spec.ts

---
+++ tests/src/typeof.spec.ts
@@ -0,0 +1,12 @@
+export = () => {
+	it("should allow typeof", () => {
+		expect(typeof {}).to.equal("object");
+		expect(typeof undefined).to.equal("undefined");
+		expect(typeof 1).to.equal("number");
+		expect(typeof "foo").to.equal("string");
+		expect(typeof true).to.equal("boolean");
+		expect(typeof []).to.equal("object");
+		expect(typeof new Frame()).to.equal("Instance");
+		expect(typeof new Vector2()).to.equal("Vector2");
+	});
+};
