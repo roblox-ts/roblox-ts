a6fe037274ad08b14f3976b2d933ae470e7e7b04
LouieK22 <LouieK22@users.noreply.github.com>
2018-11-12 21:12:04 -0600

    New error tests (#121)
    🎉 80% coverage!

--- src/test.ts
+++ src/test.ts
@@ -25,14 +25,39 @@ const compilerArgs = {
 
 /* tslint:disable:object-literal-sort-keys */
 const errorMatrix: ErrorMatrix = {
+	"diagnostic.spec.ts": {
+		message: "should not allow diagnostic errors",
+		instance: DiagnosticError,
+	},
 	"var.spec.ts": {
 		message: "should not allow var keyword",
 		instance: TranspilerError,
 		type: TranspilerErrorType.NoVarKeyword,
 	},
-	"diagnostic.spec.ts": {
-		message: "should not allow diagnostic errors",
-		instance: DiagnosticError,
+	"reservedLuaKeywords.spec.ts": {
+		message: "should not allow usage of reserved lua keywords",
+		instance: TranspilerError,
+		type: TranspilerErrorType.ReservedKeyword,
+	},
+	"breakLabel.spec.ts": {
+		message: "should not allow usage of break labels",
+		instance: TranspilerError,
+		type: TranspilerErrorType.NoLabeledStatement,
+	},
+	"continueLabel.spec.ts": {
+		message: "should not allow usage of continue labels",
+		instance: TranspilerError,
+		type: TranspilerErrorType.NoLabeledStatement,
+	},
+	"constructorReturn.spec.ts": {
+		message: "should not allow return in class constructor",
+		instance: TranspilerError,
+		type: TranspilerErrorType.NoConstructorReturn,
+	},
+	"null.spec.ts": {
+		message: "should not allow usage of null",
+		instance: TranspilerError,
+		type: TranspilerErrorType.NoNull,
 	},
 };
 /* tslint:enable:object-literal-sort-keys */
@@ -72,7 +97,7 @@ describe("compile error unit tests", () => {
 						} else if (errorMatrix[file].type === e.type) {
 							done();
 						} else {
-							done("Unexpected error");
+							done("Unexpected TranspilerErrorType");
 						}
 					} else {
 						done("Unexpected error");
