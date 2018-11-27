095729ef5f27368c13a6e8dcaae57002dcef7f75
Eryn Lynn <eryn@eryn.io>
2018-11-27 03:14:58 -0500

    Create instance_values.spec.ts

---
+++ tests/src/instance_values.spec.ts
@@ -0,0 +1,8 @@
+export = () => {
+	it("should allow passing Roblox instances as values", () => {
+		expect(Part).to.be.ok()
+
+		const part = Part
+		expect((new part()).ClassName).to.equal("Part")
+	});
+};
