9a63b9cb0d095c92296e48f410d1c75bd6b8fbe7
Osyris <osyrisrblx@gmail.com>
2018-11-30 19:36:13 -0500

    test debugging

--- src/test.ts
+++ src/test.ts
@@ -136,7 +136,7 @@ describe("compile error unit tests", () => {
 					} else if (e instanceof CompilerError) {
 						done(`Unexpected CompilerError: ${CompilerErrorType[e.type]}`);
 					} else if (e instanceof DiagnosticError) {
-						done(`Unexpected DiagnosticError:\n${e.errors.join("\n")}`);
+						done(`Unexpected DiagnosticError: ${e.errors.join(", ")}`);
 					} else {
 						done("Unexpected error: " + String(e));
 					}
