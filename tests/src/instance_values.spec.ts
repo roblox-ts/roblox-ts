94570ae9d9e11967606da03058fdea6344a2952f
Osyris <osyrisrblx@gmail.com>
2019-02-02 23:57:58 -0500

    remove instance_values.spec.ts

--- tests/src/instance_values.spec.ts
+++
@@ -1,8 +0,0 @@
-export = () => {
-	it("should allow passing Roblox instances as values", () => {
-		expect(Frame).to.be.ok();
-
-		const frame = Frame;
-		expect(new frame().ClassName).to.equal("Frame");
-	});
-};
