edf5580d76a7ea4f93b7e02cb9c8191f897388e2
Osyris <osyrisrblx@gmail.com>
2019-02-12 03:00:48 -0500

    Do not import or export types Fix #79 Fix #220

--- src/typeUtilities.ts
+++ src/typeUtilities.ts
@@ -3,10 +3,32 @@ import * as ts from "ts-morph";
 export function isType(node: ts.Node) {
 	return (
 		ts.TypeGuards.isEmptyStatement(node) ||
+		ts.TypeGuards.isTypeReferenceNode(node) ||
 		ts.TypeGuards.isTypeAliasDeclaration(node) ||
 		ts.TypeGuards.isInterfaceDeclaration(node) ||
 		(ts.TypeGuards.isAmbientableNode(node) && node.hasDeclareKeyword())
 	);
+}
+
+export function isUsedAsType(node: ts.Identifier) {
+	for (const ref of node.findReferences()) {
+		for (const ref2 of ref.getReferences()) {
+			const parent = ref2.getNode().getParent() as ts.Node;
+			if (
+				ref2.getSourceFile() === node.getSourceFile() &&
+				!ts.TypeGuards.isImportSpecifier(parent) &&
+				!ts.TypeGuards.isImportClause(parent) &&
+				!ts.TypeGuards.isImportEqualsDeclaration(parent) &&
+				!ts.TypeGuards.isExportAssignment(parent) &&
+				!ts.TypeGuards.isExportSpecifier(parent)
+			) {
+				if (!isType(parent)) {
+					return false;
+				}
+			}
+		}
+	}
+	return true;
 }
 
 export function inheritsFrom(type: ts.Type, className: string): boolean {
