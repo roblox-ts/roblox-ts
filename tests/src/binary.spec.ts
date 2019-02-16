cb310b7e7e399d0a193745a3fd59efc35beab079
Osyris <osyrisrblx@gmail.com>
2019-02-16 01:20:07 -0500

    improve tests

--- tests/src/binary.spec.ts
+++ tests/src/binary.spec.ts
@@ -6,4 +6,12 @@ export = () => {
 		foo.bar++;
 		expect(foo.bar).to.equal(2);
 	});
+
+	it("should support binary assignment on properties", () => {
+		const foo = {
+			bar: 1,
+		};
+		foo.bar += 1;
+		expect(foo.bar).to.equal(2);
+	});
 };
