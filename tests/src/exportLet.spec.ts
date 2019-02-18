b794d494393b059ef36d1cd9c18a45c1b7ed13c1
Osyris <osyrisrblx@gmail.com>
2019-02-17 19:15:03 -0500

    improve tests

--- tests/src/exportLet.spec.ts
+++ tests/src/exportLet.spec.ts
@@ -1,5 +1,7 @@
+let y = 0;
 namespace Foo {
 	export let x = 1;
+	y = x;
 }
 
 export = () => {
@@ -7,5 +9,6 @@ export = () => {
 		expect(Foo.x).to.equal(1);
 		Foo.x = 2;
 		expect(Foo.x).to.equal(2);
+		expect(y).to.equal(1);
 	});
 };
