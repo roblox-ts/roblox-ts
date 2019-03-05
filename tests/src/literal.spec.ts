13b732cccbf1694e2041b15dced3e0da6c09b761
Validark <NarrevRBX@gmail.com>
2019-03-04 20:30:44 -0600

    update test

--- tests/src/literal.spec.ts
+++ tests/src/literal.spec.ts
@@ -11,8 +11,7 @@ export = () => {
 		expect(100000000000000).never.to.equal(1);
 		expect(tostring(0.0000000000001)).to.equal("1e-13");
 		expect(tostring(100000000000000)).to.equal("1e+14");
-		expect(100_00).never.to.throw();
-		expect(100_00).to.equal(10000);
+		expect(1_0_0_0_0).to.equal(10000);
 	});
 
 	it("should add numbers", () => {
