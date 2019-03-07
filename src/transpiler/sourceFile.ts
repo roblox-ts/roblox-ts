65212c3bd92c2d00faf8358dd16d1911a32f04bc
Osyris <osyrisrblx@gmail.com>
2019-03-07 02:42:41 -0500

    add profiling

--- src/transpiler/sourceFile.ts
+++ src/transpiler/sourceFile.ts
@@ -5,6 +5,8 @@ import { TranspilerState } from "../TranspilerState";
 import { getScriptContext, getScriptType, ScriptType } from "../utility";
 
 export function transpileSourceFile(state: TranspilerState, node: ts.SourceFile) {
+	console.profile(node.getBaseName());
+
 	state.scriptContext = getScriptContext(node);
 	const scriptType = getScriptType(node);
 	let result = transpileStatementedNode(state, node);
@@ -48,5 +50,6 @@ export function transpileSourceFile(state: TranspilerState, node: ts.SourceFile)
 			result;
 	}
 
+	console.profileEnd();
 	return result;
 }
