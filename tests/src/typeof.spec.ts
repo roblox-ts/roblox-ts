9d90344c9dbab3fbb961e86c9faf83e86dc2a63c
Validark <NarrevRBX@gmail.com>
2019-03-25 22:11:18 -0500

    add test

--- tests/src/typeof.spec.ts
+++ tests/src/typeof.spec.ts
@@ -8,4 +8,14 @@ export = () => {
 		expect(typeOf([])).to.equal("table");
 		expect(typeOf(new Vector2())).to.equal("Vector2");
 	});
+
+	it("should support typeIs", () => {
+		expect(typeIs({}, "table")).to.equal(true);
+		expect(typeIs(undefined, "nil")).to.equal(true);
+		expect(typeIs(1, "number")).to.equal(true);
+		expect(typeIs("foo", "string")).to.equal(true);
+		expect(typeIs(true, "boolean")).to.equal(true);
+		expect(typeIs([], "table")).to.equal(true);
+		expect(typeIs(new Vector2(), "Vector2")).to.equal(true);
+	});
 };
