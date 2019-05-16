roblox-ts/roblox-ts

commit efdb8088f1aac20f1237d35085cdd1d63a8471b5

fix tests

src/compiler/call.ts
@@ -488,7 +488,6 @@ export function getPropertyAccessExpressionType(
 		if (STRING_MACRO_METHODS.indexOf(property) !== -1) {
 			return PropertyCallExpType.BuiltInStringMethod;
 		}
-
 		return PropertyCallExpType.String;
 	}
 

src/typeUtilities.ts
@@ -154,7 +154,7 @@ export function isStringType(type: ts.Type) {
 export function getCompilerDirectiveWithConstraint(
 	type: ts.Type,
 	directive: CompilerDirective,
-	orCallback = (t: ts.Type) => true,
+	orCallback = (t: ts.Type) => false,
 ) {
 	return typeConstraint(type, t => {
 		const symbol = t.getSymbol();

commit 6996e769fc5d9e4c8936a65d0cbae1a8231d3c95

fix array and string .length regression introduced by commit efdb808

src/compiler/call.ts
@@ -16,6 +16,8 @@ import {
 	isStringMethodType,
 	isTupleReturnTypeCall,
 	typeConstraint,
+	isArrayType,
+	isStringType,
 } from "../typeUtilities";
 
 const STRING_MACRO_METHODS = [
@@ -479,11 +481,11 @@ export function getPropertyAccessExpressionType(
 	const subExpType = subExp.getType();
 	const property = expression.getName();
 
-	if (isArrayMethodType(expType)) {
+	if (isArrayMethodType(expType) || (isArrayType(subExpType) && property == "length")) {
 		return PropertyCallExpType.Array;
 	}
 
-	if (isStringMethodType(subExpType)) {
+	if (isStringMethodType(subExpType) || (isStringType(subExpType) && property == "length")) {
 		if (STRING_MACRO_METHODS.indexOf(property) !== -1) {
 			return PropertyCallExpType.BuiltInStringMethod;
 		}