76d13eb97d126d1edfe058dfc8a3950cc7689fec
osyrisrblx <osyrisrblx@gmail.com>
2019-05-26 01:45:01 -0700

    Add support for TaggedTemplateExpression

--- src/compiler/literal.ts
+++ src/compiler/literal.ts
@@ -1,8 +1,6 @@
 import * as ts from "ts-morph";
-import { compileExpression } from ".";
+import { sanitizeTemplate } from ".";
 import { CompilerState } from "../CompilerState";
-import { isStringType } from "../typeUtilities";
-import { compileList } from "./call";
 
 export function compileBooleanLiteral(state: CompilerState, node: ts.BooleanLiteral) {
 	return node.getLiteralValue() === true ? "true" : "false";
@@ -31,56 +29,10 @@ export function compileNumericLiteral(state: CompilerState, node: ts.NumericLite
 	return text.replace(/_/g, "");
 }
 
-function sanitizeTemplate(str: string) {
-	str = str.replace(/(^|[^\\](?:\\\\)*)"/g, '$1\\"'); // replace " with \"
-	str = str.replace(/(\r?\n|\r)/g, "\\$1"); // prefix new lines with \
-	return str;
-}
-
 export function compileStringLiteral(state: CompilerState, node: ts.StringLiteral | ts.NoSubstitutionTemplateLiteral) {
 	if (ts.TypeGuards.isNoSubstitutionTemplateLiteral(node)) {
 		return '"' + sanitizeTemplate(node.getText().slice(1, -1)) + '"';
 	} else {
 		return node.getText();
 	}
-}
-
-export function compileTemplateExpression(state: CompilerState, node: ts.TemplateExpression) {
-	const followingStrs = new Map<ts.Expression, string>();
-
-	const bin = compileList(
-		state,
-		node
-			.getTemplateSpans()
-			.filter(span => ts.TypeGuards.isTemplateSpan(span))
-			.map(span => {
-				const exp = span.getExpression();
-				const literal = span.getLiteral();
-				const literalStr = sanitizeTemplate(
-					literal.getText().slice(1, ts.TypeGuards.isTemplateMiddle(literal) ? -2 : -1),
-				);
-
-				if (literalStr.length > 0) {
-					followingStrs.set(exp, ` .. "${literalStr}"`);
-				}
-				return exp;
-			}),
-		(_, exp) => {
-			const expStr = compileExpression(state, exp);
-			return (isStringType(exp.getType()) ? expStr : `tostring(${expStr})`) + (followingStrs.get(exp) || "");
-		},
-	);
-
-	const headText = sanitizeTemplate(
-		node
-			.getHead()
-			.getText()
-			.slice(1, -2),
-	);
-
-	if (headText.length > 0) {
-		bin.unshift(`"${headText}"`);
-	}
-
-	return bin.join(" .. ");
 }
