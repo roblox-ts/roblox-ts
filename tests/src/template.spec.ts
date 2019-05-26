31ff4d946fa66a2128f13046c36f44f2c70572fe
osyrisrblx <osyrisrblx@gmail.com>
2019-05-26 01:50:14 -0700

    add tests

---
+++ tests/src/template.spec.ts
@@ -0,0 +1,35 @@
+export = () => {
+	it("should support tagged template expressions", () => {
+		const OPERATIONS: { [index: string]: (a: Vector3, b: Vector3) => Vector3 } = {
+			"*": (a, b) => a.mul(b),
+			"/": (a, b) => a.div(b),
+			"+": (a, b) => a.add(b),
+			"-": (a, b) => a.sub(b),
+		};
+
+		function m(strings: TemplateStringsArray, ...operands: Vector3[]): Vector3 {
+			const operators = strings.map(v => v.trim());
+
+			let value = operands.shift()!;
+			operators.shift();
+
+			for (let i = 0; i < operands.length; i++) {
+				const operator = operators[i].trim();
+				if (operator in OPERATIONS) {
+					const operation = OPERATIONS[operator];
+					value = operation(value, operands[i]);
+				}
+			}
+
+			return value;
+		}
+
+		const a = new Vector3(1, 2, 3);
+		const b = new Vector3(4, 5, 6);
+		const pos = m`${a} * ${b} - ${new Vector3(1, 2, 3)}`;
+
+		expect(pos.X).to.equal(3);
+		expect(pos.Y).to.equal(8);
+		expect(pos.Z).to.equal(15);
+	});
+};
