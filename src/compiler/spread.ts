e544ef8ce13e9662be0311985dc03aa116f1919d
Validark <NarrevRBX@gmail.com>
2019-06-01 04:23:37 -0500

    remove unused imports

--- src/compiler/spread.ts
+++ src/compiler/spread.ts
@@ -3,7 +3,6 @@ import { checkNonAny, compileCallExpression, compileExpression } from ".";
 import { CompilerState, PrecedingStatementContext } from "../CompilerState";
 import { CompilerError, CompilerErrorType } from "../errors/CompilerError";
 import {
-	getCompilerDirectiveWithConstraint,
 	isArrayType,
 	isIterableFunction,
 	isIterableIterator,
@@ -13,7 +12,6 @@ import {
 	isTupleReturnTypeCall,
 	shouldPushToPrecedingStatement,
 } from "../typeUtilities";
-import { CompilerDirective } from "./security";
 
 export function shouldCompileAsSpreadableList(elements: Array<ts.Expression>) {
 	const { length } = elements;
