0cb78695090ac8b7055ea268d77a8e9c4fb73725
Validark <NarrevRBX@gmail.com>
2019-06-03 01:30:47 -0500

    remove tests

--- tests/src/robloxEnum.spec.ts
+++ tests/src/robloxEnum.spec.ts
@@ -1,17 +1,17 @@
+// For if Lemur ever supports Enums:
+
 export = () => {
-	it("should permit the use of GetEnums()", () => {
-		for (const enumType of Enum.GetEnums()) {
-			for (const { Name: name } of enumType.GetEnumItems()) {
-				expect(typeOf(name)).to.equal("string");
-			}
-		}
-	});
-
-	it("should permit the use of GetEnumItems()", () => {
-		const axisNames = new Set(["X", "Y", "Z"]);
-
-		for (const { Name: name } of Enum.Axis.GetEnumItems()) {
-			expect(axisNames.has(name)).to.equal(true);
-		}
-	});
+	// it("should permit the use of GetEnums()", () => {
+	// 	for (const enumType of Enum.GetEnums()) {
+	// 		for (const { Name: name } of enumType.GetEnumItems()) {
+	// 			expect(typeOf(name)).to.equal("string");
+	// 		}
+	// 	}
+	// });
+	// it("should permit the use of GetEnumItems()", () => {
+	// 	const axisNames = new Set(["X", "Y", "Z"]);
+	// 	for (const { Name: name } of Enum.Axis.GetEnumItems()) {
+	// 		expect(axisNames.has(name)).to.equal(true);
+	// 	}
+	// });
 };
