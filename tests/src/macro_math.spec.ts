ab797f3a42fa97fc8bd0d664f30aedc26b6d54f8
osyrisrblx <osyrisrblx@gmail.com>
2019-07-12 12:11:48 -0700

    update test title v2

--- tests/src/macro_math.spec.ts
+++ tests/src/macro_math.spec.ts
@@ -35,7 +35,7 @@ export = () => {
 		expect(c.Z).to.equal(2);
 	});
 
-	it("should properly support roblox math macros", () => {
+	it("should properly support roblox math macros with binary expressions", () => {
 		const a = new Vector2(1, 2).mul(1 + 3);
 		expect(a.X).to.equal(4);
 		expect(a.Y).to.equal(8);
