8e7db4694c731e0cccbf82b0264dece397e78e30
osyrisrblx <osyrisrblx@gmail.com>
2019-10-30 16:45:20 -0700

    switch to new compile format

--- src/compiler/enum.ts
+++ src/compiler/enum.ts
@@ -21,31 +21,35 @@ export function compileEnumDeclaration(state: CompilerState, node: ts.EnumDeclar
 	if (shouldHoist(node, nameNode)) {
 		state.pushHoistStack(name);
 	}
-	result += state.indent + `${name} = ${name} or {};\n`;
+	result += state.indent + `local ${name};\n`;
 	result += state.indent + `do\n`;
 	state.pushIndent();
+	state.pushIdStack();
+	const inverseId = state.getNewId();
+	result += state.indent + `local ${inverseId} = {};\n`;
+	result += state.indent + `${name} = setmetatable({}, { __index = ${inverseId} });\n`;
 	for (const member of node.getMembers()) {
 		const memberName = member.getName();
 		const memberValue = member.getValue();
 		const safeIndex = safeLuaIndex(name, memberName);
-
 		if (typeof memberValue === "string") {
 			const strForm = '"' + sanitizeTemplate(memberValue) + '"';
 			result += state.indent + `${safeIndex} = ${strForm};\n`;
 		} else if (typeof memberValue === "number") {
 			result += state.indent + `${safeIndex} = ${memberValue};\n`;
-			result += state.indent + `${name}[${memberValue}] = "${memberName}";\n`;
+			result += state.indent + `${inverseId}[${memberValue}] = "${memberName}";\n`;
 		} else if (member.hasInitializer()) {
 			const initializer = skipNodesDownwards(member.getInitializerOrThrow());
 			state.enterPrecedingStatementContext();
 			const expStr = getReadableExpressionName(state, initializer);
 			result += state.exitPrecedingStatementContextAndJoin();
 			result += state.indent + `${safeIndex} = ${expStr};\n`;
-			result += state.indent + `${name}[${expStr}] = "${memberName}";\n`;
+			result += state.indent + `${inverseId}[${expStr}] = "${memberName}";\n`;
 		} else {
 			throw new CompilerError("Unexpected enum structure.", node, CompilerErrorType.BadEnum, true);
 		}
 	}
+	state.popIdStack();
 	state.popIndent();
 	result += state.indent + `end;\n`;
 	return result;
