4d4919d742f0f9a9e0ed8ca6dd1e1eefb29c29c0
osyrisrblx <osyrisrblx@gmail.com>
2020-01-27 21:32:18 -0800

    skip nested parentheses

--- src/LuaRenderer/nodes/expressions/indexable/parenthesizedExpression.ts
+++ src/LuaRenderer/nodes/expressions/indexable/parenthesizedExpression.ts
@@ -3,5 +3,10 @@ import * as lua from "../../../../LuaAST";
 import { RenderState } from "../../../RenderState";
 
 export function renderParenthesizedExpression(state: RenderState, node: lua.ParenthesizedExpression) {
+	// skip nested parentheses
+	let expression = node.expression;
+	while (lua.isParenthesizedExpression(expression)) {
+		expression = expression.expression;
+	}
 	return `(${render(state, node.expression)})`;
 }
