e78f47a50789d7b875a3e4cf168430ca046b1482
osyrisrblx <osyrisrblx@gmail.com>
2020-01-28 11:22:58 -0800

    Make function expressions a single line if they contain no statements

--- src/LuaRenderer/nodes/expressions/functionExpression.ts
+++ src/LuaRenderer/nodes/expressions/functionExpression.ts
@@ -4,6 +4,10 @@ import { renderParameters } from "LuaRenderer/util/parameters";
 import { renderStatements } from "LuaRenderer/util/statements";
 
 export function renderFunctionExpression(state: RenderState, node: lua.FunctionExpression) {
+	if (!node.statements.head) {
+		return `function(${renderParameters(state, node)}) end`;
+	}
+
 	let result = "";
 
 	result += `function(${renderParameters(state, node)})\n`;
--- src/LuaRenderer/test.ts
+++ src/LuaRenderer/test.ts
@@ -6,43 +6,12 @@ import * as path from "path";
 
 const ast = lua.list.make<lua.Statement>();
 
-lua.list.push(ast, lua.varDec("Workspace", lua.methodCallExp(lua.id("game"), "GetService", [lua.string("Workspace")])));
-
-lua.list.push(
-	ast,
-	lua.create(lua.SyntaxKind.IfStatement, {
-		condition: lua.bool(true),
-		statements: lua.list.make(lua.call(lua.id("print"), [lua.number(1), lua.number(2), lua.number(3)])),
-		elseBody: lua.create(lua.SyntaxKind.IfStatement, {
-			condition: lua.bool(true),
-			statements: lua.list.make(lua.call(lua.id("print"), [lua.number(1), lua.number(2), lua.number(3)])),
-			elseBody: lua.create(lua.SyntaxKind.IfStatement, {
-				condition: lua.bool(true),
-				statements: lua.list.make(lua.call(lua.id("print"), [lua.number(1), lua.number(2), lua.number(3)])),
-				elseBody: lua.list.make(lua.call(lua.id("print"), [lua.number(1), lua.number(2), lua.number(3)])),
-			}),
-		}),
-	}),
-);
-
-lua.list.push(
-	ast,
-	lua.whileDo(lua.bool(true), [lua.call(lua.id("print"), [lua.number(1), lua.number(2), lua.number(3)])]),
-);
-
-lua.list.push(
-	ast,
-	lua.funcDec("foo", [lua.id("bar")], false, [
-		lua.varDec("Workspace", lua.methodCallExp(lua.id("game"), "GetService", [lua.string("Workspace")])),
-	]),
-);
-
-lua.list.push(ast, lua.call(lua.id("print"), []));
-lua.list.push(ast, lua.call(lua.parentheses(lua.id("print")), []));
-
-lua.list.push(ast, lua.comment("foobar!"));
-
-lua.list.push(ast, lua.methodCall(lua.string("foo"), "bar", []));
+lua.list.push(ast, lua.call(lua.func(), []));
+lua.list.push(ast, lua.call(lua.func(), []));
+lua.list.push(ast, lua.call(lua.func(), []));
+lua.list.push(ast, lua.call(lua.func(), []));
+lua.list.push(ast, lua.call(lua.func(), []));
+lua.list.push(ast, lua.call(lua.func(), []));
 
 const luaSource = renderStatements(new RenderState(), ast);
 fs.writeFileSync(path.resolve(__dirname, "..", "..", "out.lua"), luaSource);
