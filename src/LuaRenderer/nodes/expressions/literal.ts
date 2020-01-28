1699f9fd728c5977c52ae247dd0c9fdeab16b751
osyrisrblx <osyrisrblx@gmail.com>
2020-01-28 13:11:28 -0800

    update renderStringLiteral

--- src/LuaRenderer/nodes/expressions/literal.ts
+++ src/LuaRenderer/nodes/expressions/literal.ts
@@ -5,7 +5,17 @@ export function renderNumberLiteral(state: RenderState, node: lua.NumberLiteral)
 	return `${node.value}`;
 }
 
-export function renderStringLiteral(render: RenderState, node: lua.StringLiteral) {
-	// TODO: resolve " vs ' vs [[]]
-	return `"${node.value}"`;
+export function renderStringLiteral(state: RenderState, node: lua.StringLiteral) {
+	if (!node.value.includes('"')) {
+		return `"${node.value}"`;
+	} else if (!node.value.includes("'")) {
+		return `'${node.value}'`;
+	} else {
+		let amtEquals = 0;
+		while (!node.value.includes(`]${"=".repeat(amtEquals)}]`)) {
+			amtEquals++;
+		}
+		const eqStr = "=".repeat(amtEquals);
+		return `[${eqStr}[${node.value}]${eqStr}]`;
+	}
 }
