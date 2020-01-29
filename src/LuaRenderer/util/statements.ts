ad8c707e2eb93004becbb3010f9cb342c6bbfa0c
osyrisrblx <osyrisrblx@gmail.com>
2020-01-28 17:09:01 -0800

    whitespace

--- src/LuaRenderer/util/statements.ts
+++ src/LuaRenderer/util/statements.ts
@@ -4,16 +4,12 @@ import { RenderState } from "LuaRenderer";
 
 export function renderStatements(state: RenderState, statements: lua.List<lua.Statement>) {
 	let result = "";
-
 	let listNode = statements.head;
 	while (listNode !== undefined) {
 		state.pushListNode(listNode);
-
 		result += render(state, listNode.value);
-
 		listNode = listNode.next;
 		state.popListNode();
 	}
-
 	return result;
 }
