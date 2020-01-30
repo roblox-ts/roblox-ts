1ed5344385dfd357fe10a20204731c53fb180654
osyrisrblx <osyrisrblx@gmail.com>
2020-01-30 00:03:02 -0800

    update CLIError.ts

--- src/CLI/errors/CLIError.ts
+++ src/CLI/errors/CLIError.ts
@@ -1,7 +1,15 @@
+import chalk from "chalk";
 import { LoggableError } from "Shared/errors/LoggableError";
 
+// force colors
+chalk.level = 1;
+
 export class CLIError extends LoggableError {
-	log() {
-		console.log(`CLI Error: ${this.message}`);
+	constructor(private message: string) {
+		super();
+	}
+
+	public log() {
+		console.log(chalk.redBright("CLI Error:"), this.message);
 	}
 }
