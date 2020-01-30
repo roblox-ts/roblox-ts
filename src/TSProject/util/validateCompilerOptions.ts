1f14ebb21f772ab4434db3a57dbdcd0226cd027d
osyrisrblx <osyrisrblx@gmail.com>
2020-01-30 12:41:02 -0800

    update validateCompilerOptions.ts

--- src/TSProject/util/validateCompilerOptions.ts
+++ src/TSProject/util/validateCompilerOptions.ts
@@ -40,25 +40,32 @@ export function validateCompilerOptions(
 	if (opts.downlevelIteration !== ENFORCED_OPTIONS.downlevelIteration) {
 		errors.push(`${y(`"downlevelIteration"`)} must be ${y(`true`)}`);
 	}
+
 	if (opts.module !== ENFORCED_OPTIONS.module) {
 		errors.push(`${y(`"module"`)} must be ${y(`"commonjs"`)}`);
 	}
+
 	if (opts.noLib !== ENFORCED_OPTIONS.noLib) {
 		errors.push(`${y(`"noLib"`)} must be ${y(`true`)}`);
 	}
+
 	if (opts.strict !== ENFORCED_OPTIONS.strict) {
 		errors.push(`${y(`"strict"`)} must be ${y(`true`)}`);
 	}
+
 	if (opts.target !== ENFORCED_OPTIONS.target) {
 		errors.push(`${y(`"target"`)} must be ${y(`"es6"`)}`);
 	}
+
 	if (opts.allowSyntheticDefaultImports !== ENFORCED_OPTIONS.allowSyntheticDefaultImports) {
 		errors.push(`${y(`"allowSyntheticDefaultImports"`)} must be ${y(`true`)}`);
 	}
+
 	if (opts.isolatedModules !== ENFORCED_OPTIONS.isolatedModules) {
 		errors.push(`${y(`"isolatedModules"`)} must be ${y(`true`)}`);
 	}
 
+	// validate typeRoots
 	let typesFound = false;
 	if (opts.typeRoots) {
 		const typesPath = path.resolve(modulesPath, "@rbxts");
@@ -78,6 +85,7 @@ export function validateCompilerOptions(
 	if (opts.rootDir === undefined && opts.rootDirs === undefined) {
 		errors.push(`${y(`"rootDir"`)} or ${y(`"rootDirs"`)} must be defined`);
 	}
+
 	if (opts.outDir === undefined) {
 		errors.push(`${y(`"outDir"`)} must be defined`);
 	}
@@ -86,6 +94,7 @@ export function validateCompilerOptions(
 	if (opts.jsx !== undefined && opts.jsx !== ts.JsxEmit.React) {
 		errors.push(`${y(`"jsx"`)} must be ${y(`"react"`)} or not defined`);
 	}
+
 	if (opts.jsxFactory !== undefined && opts.jsxFactory !== "Roact.createElement") {
 		errors.push(`${y(`"jsxFactory"`)} must be ${y(`"Roact.createElement"`)} or not defined`);
 	}
@@ -95,7 +104,7 @@ export function validateCompilerOptions(
 		throw new ProjectError(
 			[
 				`Invalid "tsconfig.json" configuration!`,
-				`https://roblox-ts.github.io/docs/quick-start#project-folder-setup`,
+				`https://roblox-ts.github.io/docs/quick-start#project-folder-setup`, // TODO update
 				errors.map(e => `- ${e}\n`).join(""),
 			].join("\n"),
 		);
