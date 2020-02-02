9ac2e102f651794eb2977a4a28d389b96162521c
osyrisrblx <osyrisrblx@gmail.com>
2020-02-01 17:03:05 -0800

    update test.ts

--- src/LuaRenderer/test.ts
+++ src/LuaRenderer/test.ts
@@ -9,8 +9,8 @@ const ast = lua.list.make<lua.Statement>();
 lua.list.push(ast, lua.comment("Case #1"));
 lua.list.push(ast, lua.call(lua.parentheses(lua.func())));
 lua.list.push(ast, lua.call(lua.parentheses(lua.func())));
-lua.list.push(ast, lua.call(lua.id("print")));
-lua.list.push(ast, lua.call(lua.id("print")));
+lua.list.push(ast, lua.comment("Case #2"));
+lua.list.push(ast, lua.call(lua.callExp(lua.callExp(lua.parentheses(lua.func())), [lua.func()])));
 
 const luaSource = renderStatements(new RenderState(), ast);
 fs.writeFileSync(path.resolve(__dirname, "..", "..", "out.lua"), luaSource);
