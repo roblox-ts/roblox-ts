3c8988e9df2bb48eb65f53f89b411b2efc41d810
osyrisrblx <osyrisrblx@gmail.com>
2020-02-01 17:50:07 -0800

    Add case for isCallExpression or isMethodCallExpression to getEnding

--- src/LuaRenderer/util/getEnding.ts
+++ src/LuaRenderer/util/getEnding.ts
@@ -19,6 +19,9 @@ function startsWithParenthesisInner(node: lua.Expression): boolean {
 	if (lua.isParenthesizedExpression(node)) {
 		// (a)
 		return true;
+	} else if (lua.isCallExpression(node) || lua.isMethodCallExpression(node)) {
+		// (a)() || (a):b()
+		return startsWithParenthesisInner(node.expression);
 	} else if (lua.isPropertyAccessExpression(node) || lua.isComputedIndexExpression(node)) {
 		// (a).b or (a)[b]
 		return startsWithParenthesisInner(node.expression);
