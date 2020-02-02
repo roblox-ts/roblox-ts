1b80a1f770a0239c8ea8dc4cdfd6d15aead00b3c
osyrisrblx <osyrisrblx@gmail.com>
2020-02-02 01:56:40 -0800

    remove unnecessary args

--- src/TSTransformer/test.ts
+++ src/TSTransformer/test.ts
@@ -3,13 +3,7 @@ import { transformSourceFile } from "TSTransformer/nodes/sourceFile";
 import ts from "typescript";
 import { renderAST } from "LuaRenderer";
 
-const sourceFile = ts.createSourceFile(
-	"test.ts",
-	"const x = 1; const y = 2; const z = 3;",
-	ts.ScriptTarget.ES2017,
-	true,
-	ts.ScriptKind.TS,
-);
+const sourceFile = ts.createSourceFile("test.ts", "const x = 1; const y = 2; const z = 3;", ts.ScriptTarget.ES2017);
 
 const luaAST = transformSourceFile(new TransformState(), sourceFile);
 const luaSource = renderAST(luaAST);
