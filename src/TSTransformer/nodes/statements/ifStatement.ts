d02ab0d080a7adcbef5e7cb5a7b382968d940bd6
osyrisrblx <osyrisrblx@gmail.com>
2020-02-03 21:51:41 -0800

    Add ifStatement.ts

---
+++ src/TSTransformer/nodes/statements/ifStatement.ts
@@ -0,0 +1,42 @@
+import * as lua from "LuaAST";
+import { transformStatement } from "TSTransformer/nodes/statements/statement";
+import { TransformState } from "TSTransformer/TransformState";
+import { transformConditional } from "TSTransformer/util/transformConditional";
+import { transformStatementList } from "TSTransformer/util/transformStatementList";
+import ts from "typescript";
+
+function transformElseStatement(
+	state: TransformState,
+	elseStatement: ts.Statement | undefined,
+): lua.IfStatement | lua.List<lua.Statement> {
+	if (elseStatement === undefined) {
+		return lua.list.make<lua.Statement>();
+	} else if (ts.isIfStatement(elseStatement)) {
+		return transformIfStatementInner(state, elseStatement);
+	} else if (ts.isBlock(elseStatement)) {
+		return transformStatementList(state, elseStatement.statements);
+	} else {
+		return transformStatement(state, elseStatement);
+	}
+}
+
+export function transformIfStatementInner(state: TransformState, node: ts.IfStatement) {
+	const condition = transformConditional(state, node.expression);
+
+	const statements = transformStatementList(
+		state,
+		ts.isBlock(node.thenStatement) ? node.thenStatement.statements : [node.thenStatement],
+	);
+
+	const elseBody = transformElseStatement(state, node.elseStatement);
+
+	return lua.create(lua.SyntaxKind.IfStatement, {
+		condition,
+		statements,
+		elseBody,
+	});
+}
+
+export function transformIfStatement(state: TransformState, node: ts.IfStatement) {
+	return lua.list.make(transformIfStatementInner(state, node));
+}
