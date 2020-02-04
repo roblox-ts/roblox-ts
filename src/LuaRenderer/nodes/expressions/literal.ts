bef505b7bdb7e4bdc8127caed8bbdb7154b920be
osyrisrblx <osyrisrblx@gmail.com>
2020-02-03 21:50:58 -0800

    update renderStringLiteral

--- src/LuaRenderer/nodes/expressions/literal.ts
+++ src/LuaRenderer/nodes/expressions/literal.ts
@@ -7,14 +7,14 @@ export function renderNumberLiteral(state: RenderState, node: lua.NumberLiteral)
 }
 
 export function renderStringLiteral(state: RenderState, node: lua.StringLiteral) {
-	const isMultiline = !node.value.includes("\n");
+	const isMultiline = node.value.includes("\n");
 	if (!isMultiline && !node.value.includes('"')) {
 		return `"${node.value}"`;
 	} else if (!isMultiline && !node.value.includes("'")) {
 		return `'${node.value}'`;
 	} else {
 		let amtEquals = 0;
-		while (!node.value.includes(`]${"=".repeat(amtEquals)}]`)) {
+		while (node.value.includes(`]${"=".repeat(amtEquals)}]`)) {
 			amtEquals++;
 		}
 		const eqStr = "=".repeat(amtEquals);
