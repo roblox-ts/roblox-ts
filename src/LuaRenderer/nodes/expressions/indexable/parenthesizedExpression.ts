b095d4824906a4663f65549f710e9f5ad83063e3
osyrisrblx <osyrisrblx@gmail.com>
2020-02-04 16:33:34 -0800

    skip useless parentheses?

--- src/LuaRenderer/nodes/expressions/indexable/parenthesizedExpression.ts
+++ src/LuaRenderer/nodes/expressions/indexable/parenthesizedExpression.ts
@@ -7,5 +7,8 @@ export function renderParenthesizedExpression(state: RenderState, node: lua.Pare
 	while (lua.isParenthesizedExpression(expression)) {
 		expression = expression.expression;
 	}
+	if (lua.isParenthesizedExpression(node.expression) || lua.isIdentifier(node.expression)) {
+		return render(state, node.expression);
+	}
 	return `(${render(state, node.expression)})`;
 }
