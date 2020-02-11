65ea7d3812ddb98c1115e63b87b4a34b09bd0bf2
osyrisrblx <osyrisrblx@gmail.com>
2020-02-11 00:08:10 -0800

    improve binaryExpressionChain

--- src/TSTransformer/util/binaryExpressionChain.ts
+++ src/TSTransformer/util/binaryExpressionChain.ts
@@ -3,14 +3,15 @@ import * as lua from "LuaAST";
 export function binaryExpressionChain(
 	expressions: Array<lua.Expression>,
 	operator: lua.BinaryOperator,
+	index = 0,
 ): lua.Expression {
-	if (expressions.length === 1) {
-		return expressions[0];
+	if (index === expressions.length - 1) {
+		return expressions[index];
 	} else {
 		return lua.create(lua.SyntaxKind.BinaryExpression, {
-			left: expressions[0],
+			left: expressions[index],
 			operator,
-			right: binaryExpressionChain(expressions.slice(1), operator),
+			right: binaryExpressionChain(expressions, operator, index + 1),
 		});
 	}
 }
