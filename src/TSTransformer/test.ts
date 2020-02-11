58d23d0aa0af87b3305e29d8564c19c752f46870
osyrisrblx <osyrisrblx@gmail.com>
2020-02-10 22:37:27 -0800

    update test.ts

--- src/TSTransformer/test.ts
+++ src/TSTransformer/test.ts
@@ -9,7 +9,6 @@ import ts from "typescript";
 
 const FILE_NAME = "test.ts";
 const tsSource = fs.readFileSync(path.resolve(__dirname, "..", "..", "input")).toString();
-
 const sourceFile = ts.createSourceFile(FILE_NAME, tsSource, ts.ScriptTarget.ES2017);
 
 const program = ts.createProgram({
@@ -24,15 +23,8 @@ const program = ts.createProgram({
 	},
 	rootNames: ["test.ts"],
 	host: {
-		fileExists(fileName) {
-			return fileName === FILE_NAME;
-		},
-
-		getSourceFile: fileName => {
-			if (fileName === FILE_NAME) {
-				return sourceFile;
-			}
-		},
+		fileExists: fileName => fileName === FILE_NAME,
+		getSourceFile: fileName => (fileName === FILE_NAME ? sourceFile : undefined),
 		readFile: () => undefined,
 		getDefaultLibFileName: () => "",
 		writeFile: () => {},
@@ -44,7 +36,6 @@ const program = ts.createProgram({
 });
 
 const luaAST = transformSourceFile(new TransformState(program.getTypeChecker()), sourceFile);
-
 fs.writeFileSync(path.resolve(__dirname, "..", "..", "ast.json"), lua.visualizeAST(luaAST));
 
 const luaSource = renderAST(luaAST);
