a11ba76144f2f4db0a46efcc5eba858268fb37eb
osyrisrblx <osyrisrblx@gmail.com>
2020-02-13 00:58:59 -0800

    prototyping

--- src/TSTransformer/util/ensureTransformOrder.ts
+++ src/TSTransformer/util/ensureTransformOrder.ts
@@ -4,26 +4,36 @@ import { TransformState } from "TSTransformer";
 import { pushToVar } from "TSTransformer/util/pushToVar";
 
 /**
- * Takes an array of `ts.Expression` and transforms each, capturing prereqs. Returns the transformed nodes.
+ * Takes an array of transformers `() => lua.Expression` and calls each, capturing prereqs. Returns the transformed nodes.
  * Ensures the `lua.Expression` nodes execute in the same order as the `ts.Expression` nodes.
  */
 export function ensureTransformOrder<A extends lua.Expression, B extends lua.Expression>(
 	state: TransformState,
 	transformers: [() => A, () => B],
 ): [A, B];
+export function ensureTransformOrder<A extends lua.Expression, B extends lua.Expression, C extends lua.Expression>(
+	state: TransformState,
+	transformers: [() => A, () => B, () => C],
+): [A, B, C];
+export function ensureTransformOrder<
+	A extends lua.Expression,
+	B extends lua.Expression,
+	C extends lua.Expression,
+	D extends lua.Expression
+>(state: TransformState, transformers: [() => A, () => B, () => C, () => D]): [A, B, C, D];
 export function ensureTransformOrder<T extends lua.Expression>(
 	state: TransformState,
 	transformers: Array<() => T>,
 ): Array<T>;
 export function ensureTransformOrder(state: TransformState, transformers: Array<() => lua.Expression>) {
-	const expressionInfoList = transformers.map(transformer => state.capturePrereqs(() => transformer()));
+	const expressionInfoList = transformers.map(transformer => state.capturePrereqs(transformer));
 	const lastArgWithPrereqsIndex = findLastIndex(expressionInfoList, info => !lua.list.isEmpty(info.statements));
 	const result = new Array<lua.Expression>();
 	for (let i = 0; i < expressionInfoList.length; i++) {
 		const info = expressionInfoList[i];
 		state.prereqList(info.statements);
 		let expression = info.expression;
-		if (i <= lastArgWithPrereqsIndex && !lua.isTemporaryIdentifier(expression)) {
+		if (i < lastArgWithPrereqsIndex && !lua.isTemporaryIdentifier(expression)) {
 			expression = pushToVar(state, expression);
 		}
 		result.push(expression);
