83c5bc3963fc82b1832862eb355ba951f7a0fd0a
osyrisrblx <osyrisrblx@gmail.com>
2020-02-15 22:40:48 -0800

    ignore declare statements

--- src/TSTransformer/nodes/statements/transformStatement.ts
+++ src/TSTransformer/nodes/statements/transformStatement.ts
@@ -13,22 +13,27 @@ function isTypeStatement(node: ts.Statement) {
 	return ts.isInterfaceDeclaration(node) || ts.isTypeAliasDeclaration(node);
 }
 
+function isDeclaredStatement(node: ts.Statement) {
+	for (const modifier of node.modifiers ?? []) {
+		if (modifier.kind === ts.SyntaxKind.DeclareKeyword) {
+			return true;
+		}
+	}
+	return false;
+}
+
 export function transformStatement(state: TransformState, node: ts.Statement): lua.List<lua.Statement> {
 	const result = lua.list.make<lua.Statement>();
 
-	for (const comment of state.getLeadingComments(node) ?? []) {
-		lua.list.push(result, lua.comment(comment));
-	}
-
-	if (isTypeStatement(node)) return result;
+	if (isTypeStatement(node) || isDeclaredStatement(node)) return result;
 
 	if (false) throw "";
-	else if (ts.isVariableStatement(node)) lua.list.pushList(result, transformVariableStatement(state, node));
+	else if (ts.isBlock(node)) lua.list.pushList(result, transformBlock(state, node));
 	else if (ts.isExpressionStatement(node)) lua.list.pushList(result, transformExpressionStatement(state, node));
 	else if (ts.isFunctionDeclaration(node)) lua.list.pushList(result, transformFunctionDeclaration(state, node));
-	else if (ts.isReturnStatement(node)) lua.list.pushList(result, transformReturnStatement(state, node));
 	else if (ts.isIfStatement(node)) lua.list.pushList(result, transformIfStatement(state, node));
-	else if (ts.isBlock(node)) lua.list.pushList(result, transformBlock(state, node));
+	else if (ts.isReturnStatement(node)) lua.list.pushList(result, transformReturnStatement(state, node));
+	else if (ts.isVariableStatement(node)) lua.list.pushList(result, transformVariableStatement(state, node));
 	else throw new Error(`Unknown statement: ${getKindName(node)}`);
 
 	return result;
