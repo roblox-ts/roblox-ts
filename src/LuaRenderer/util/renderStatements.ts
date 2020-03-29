ad652cb5b7f76399b97ee222ba8d8ce3b5547921
osyrisrblx <osyrisrblx@gmail.com>
2020-03-29 00:23:05 -0700

    Update out of date comment

--- src/LuaRenderer/util/renderStatements.ts
+++ src/LuaRenderer/util/renderStatements.ts
@@ -3,10 +3,6 @@ import { render, RenderState } from "LuaRenderer";
 
 /**
  * Renders the given list of statements.
- *
- * Pushes each statement onto the state.statementStack as it gets
- * rendered to give context to other statements as they render.
- * Useful for getting the next or previous sibling statement.
  */
 export function renderStatements(state: RenderState, statements: lua.List<lua.Statement>) {
 	let result = "";
