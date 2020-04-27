93fdacef964c7a0cd36366a6d066c44594e2ae63
osyrisrblx <osyrisrblx@gmail.com>
2020-04-26 21:37:41 -0700

    convert OptionalChainItem to be generic

--- src/TSTransformer/util/optionalChain.ts
+++ src/TSTransformer/util/optionalChain.ts
@@ -25,41 +25,42 @@ enum OptionalChainItemKind {
 	ElementCall,
 }
 
-interface OptionalChainItem {
+interface OptionalChainItem<T extends OptionalChainItemKind, U extends ts.Expression> {
+	kind: T;
+	node: U;
 	optional: boolean;
 	type: ts.Type;
 }
 
-interface PropertyAccessItem extends OptionalChainItem {
-	node: ts.PropertyAccessExpression;
-	kind: OptionalChainItemKind.PropertyAccess;
+interface PropertyAccessItem
+	extends OptionalChainItem<OptionalChainItemKind.PropertyAccess, ts.PropertyAccessExpression> {
 	name: string;
 }
 
-interface ElementAccessItem extends OptionalChainItem {
-	node: ts.ElementAccessExpression;
-	kind: OptionalChainItemKind.ElementAccess;
+interface ElementAccessItem extends OptionalChainItem<OptionalChainItemKind.ElementAccess, ts.ElementAccessExpression> {
 	expression: ts.Expression;
 }
 
-interface CallItem extends OptionalChainItem {
-	node: ts.CallExpression;
-	kind: OptionalChainItemKind.Call;
+interface CallItem extends OptionalChainItem<OptionalChainItemKind.Call, ts.CallExpression> {
 	args: ReadonlyArray<ts.Expression>;
 }
 
-interface PropertyCallItem extends OptionalChainItem {
-	node: ts.CallExpression & { expression: ts.PropertyAccessExpression };
-	kind: OptionalChainItemKind.PropertyCall;
+interface PropertyCallItem
+	extends OptionalChainItem<
+		OptionalChainItemKind.PropertyCall,
+		ts.CallExpression & { expression: ts.PropertyAccessExpression }
+	> {
 	name: string;
 	callOptional: boolean;
 	callType: ts.Type;
 	args: ReadonlyArray<ts.Expression>;
 }
 
-interface ElementCallItem extends OptionalChainItem {
-	node: ts.CallExpression & { expression: ts.ElementAccessExpression };
-	kind: OptionalChainItemKind.ElementCall;
+interface ElementCallItem
+	extends OptionalChainItem<
+		OptionalChainItemKind.ElementAccess,
+		ts.CallExpression & { expression: ts.ElementAccessExpression }
+	> {
 	expression: ts.Expression;
 	callOptional: boolean;
 	callType: ts.Type;
