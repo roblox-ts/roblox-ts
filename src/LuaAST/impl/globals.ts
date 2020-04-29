7a9fc266246f8b951e3462fbe79c99be1e5d2279
osyrisrblx <osyrisrblx@gmail.com>
2020-04-29 13:08:20 -0700

    Add lua.globals.string.gmatch

--- src/LuaAST/impl/globals.ts
+++ src/LuaAST/impl/globals.ts
@@ -1,6 +1,7 @@
 import * as lua from "LuaAST";
 
 const TABLE_ID = lua.id("table");
+const STRING_ID = lua.id("string");
 
 function property(id: lua.Identifier, name: string) {
 	return lua.create(lua.SyntaxKind.PropertyAccessExpression, { expression: id, name: name });
@@ -13,6 +14,9 @@ export const globals = {
 	setmetatable: lua.id("setmetatable"),
 	tostring: lua.id("tostring"),
 	typeof: lua.id("typeof"),
+	string: {
+		gmatch: property(STRING_ID, "gmatch"),
+	},
 	table: {
 		create: property(TABLE_ID, "create"),
 		concat: property(TABLE_ID, "concat"),
