ed183b138d938bbd52f1e8c33e69bd8cdebfcfcb
osyrisrblx <osyrisrblx@gmail.com>
2020-04-28 20:59:47 -0700

    update comment

--- src/Shared/util/assert.ts
+++ src/Shared/util/assert.ts
@@ -1,7 +1,5 @@
 /**
  * asserts a value's truthiness, stops the debugger on failure
- * @param value value to be asserted
- * @param message optional message for error
  */
 export function assert(value: unknown, message?: string): asserts value {
 	if (!value) {
