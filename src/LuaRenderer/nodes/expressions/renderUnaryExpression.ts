62b091bb8bbde0ebed32a78a93d84c5cc4d7b362
osyrisrblx <osyrisrblx@gmail.com>
2020-05-01 00:32:38 -0700

    comments

--- src/LuaRenderer/nodes/expressions/renderUnaryExpression.ts
+++ src/LuaRenderer/nodes/expressions/renderUnaryExpression.ts
@@ -2,22 +2,30 @@ import * as lua from "LuaAST";
 import { render, RenderState } from "LuaRenderer";
 
 function needsParentheses(expression: lua.Expression, operator: lua.UnaryOperator) {
+	// #{} and -{} are invalid
 	if ((operator === "#" || operator === "-") && lua.isTable(expression)) {
 		return true;
 	}
+
+	// we should do -(a + b) instead of -a + b
 	if (lua.isBinaryExpression(expression)) {
 		return true;
 	}
+
 	return false;
 }
 
 function needsSpace(expression: lua.Expression, operator: lua.UnaryOperator) {
+	// not always needs a space
 	if (operator === "not") {
 		return true;
 	}
+
+	// "--" will create a comment!
 	if (lua.isUnaryExpression(expression) && expression.operator === "-") {
 		return true;
 	}
+
 	return false;
 }
 
