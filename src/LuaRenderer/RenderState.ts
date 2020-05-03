63539ed61eeda9646958357c75d4efed34ad733e
osyrisrblx <osyrisrblx@gmail.com>
2020-05-03 00:00:19 -0700

    Clean up RenderState

--- src/LuaRenderer/RenderState.ts
+++ src/LuaRenderer/RenderState.ts
@@ -4,6 +4,9 @@ import { getOrSetDefault } from "Shared/util/getOrSetDefault";
 
 export class RenderState {
 	public indent = "";
+	private scopeStack: Array<number> = [0];
+	private seenTempNodes = new Map<lua.TemporaryIdentifier, string>();
+	private readonly listNodesStack = new Array<lua.ListNode<lua.Statement>>();
 
 	private pushIndent() {
 		this.indent += "\t";
@@ -13,47 +16,19 @@ export class RenderState {
 		this.indent = this.indent.substr(1);
 	}
 
-	public block<T>(callback: () => T) {
-		this.pushIndent();
-		const result = callback();
-		this.popIndent();
-		return result;
-	}
-
-	private scopeStack: Array<number> = [0];
-
 	public pushScope() {
-		this.scopeStack.push(this.peekScopeStackOrThrow());
+		const top = this.scopeStack[this.scopeStack.length - 1];
+		assert(top);
+		this.scopeStack.push(top);
 	}
 
 	public popScope() {
 		this.scopeStack.pop();
 	}
 
-	private peekScopeStack() {
-		return this.scopeStack[this.scopeStack.length - 1];
-	}
-
-	private peekScopeStackOrThrow() {
-		const result = this.peekScopeStack();
-		assert(result);
-		return result;
-	}
-
-	public scope<T>(callback: () => T) {
-		this.pushScope();
-		const result = this.block(callback);
-		this.popScope();
-		return result;
-	}
-
-	private seenTempNodes = new Map<lua.TemporaryIdentifier, string>();
-
 	public getTempName(node: lua.TemporaryIdentifier) {
 		return getOrSetDefault(this.seenTempNodes, node, () => `_${this.scopeStack[this.scopeStack.length - 1]++}`);
 	}
-
-	private readonly listNodesStack = new Array<lua.ListNode<lua.Statement>>();
 
 	public pushListNode(listNode: lua.ListNode<lua.Statement>) {
 		this.listNodesStack.push(listNode);
@@ -65,5 +40,19 @@ export class RenderState {
 
 	public popListNode() {
 		return this.listNodesStack.pop();
+	}
+
+	public block<T>(callback: () => T) {
+		this.pushIndent();
+		const result = callback();
+		this.popIndent();
+		return result;
+	}
+
+	public scope<T>(callback: () => T) {
+		this.pushScope();
+		const result = this.block(callback);
+		this.popScope();
+		return result;
 	}
 }
