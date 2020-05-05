7bb4d7d9db03fdacd8447143f50e46b9a015b2ae
Noah Crowley <noahwillcrow@gmail.com>
2020-05-05 00:41:51 -0400

    Add transformTemplateExpression

---
+++ src/TSTransformer/nodes/expressions/transformTemplateExpression.ts
@@ -0,0 +1,39 @@
+import * as lua from "LuaAST";
+import ts from "byots";
+import { TransformState } from "TSTransformer/TransformState";
+import { transformExpression } from "TSTransformer/nodes/expressions/transformExpression";
+import { binaryExpressionChain } from "TSTransformer/util/binaryExpressionChain";
+
+export function transformTemplateExpression(state: TransformState, node: ts.TemplateExpression) {
+	if (node.templateSpans.length === 0) {
+		return lua.create(lua.SyntaxKind.StringLiteral, {
+			value: node.head.text,
+		});
+	}
+
+	const expressions = lua.list.make<lua.Expression>();
+
+	if (node.head.text.length > 0) {
+		lua.list.push(
+			expressions,
+			lua.create(lua.SyntaxKind.StringLiteral, {
+				value: node.head.text,
+			}),
+		);
+	}
+
+	for (const templateSpan of node.templateSpans) {
+		lua.list.push(expressions, transformExpression(state, templateSpan.expression));
+
+		if (templateSpan.literal.text.length > 0) {
+			lua.list.push(
+				expressions,
+				lua.create(lua.SyntaxKind.StringLiteral, {
+					value: templateSpan.literal.text,
+				}),
+			);
+		}
+	}
+
+	return binaryExpressionChain(lua.list.toArray(expressions), "..");
+}
