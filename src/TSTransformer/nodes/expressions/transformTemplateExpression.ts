076aa00a24881a802c24fbb41d67a45ea0bcb9ca
Noah Crowley <noahwillcrow@gmail.com>
2020-05-05 01:37:52 -0400

    tostring as necessary

--- src/TSTransformer/nodes/expressions/transformTemplateExpression.ts
+++ src/TSTransformer/nodes/expressions/transformTemplateExpression.ts
@@ -3,6 +3,7 @@ import ts from "byots";
 import { TransformState } from "TSTransformer/TransformState";
 import { transformExpression } from "TSTransformer/nodes/expressions/transformExpression";
 import { binaryExpressionChain } from "TSTransformer/util/binaryExpressionChain";
+import { isStringType } from "TSTransformer/util/types";
 
 export function transformTemplateExpression(state: TransformState, node: ts.TemplateExpression) {
 	if (node.templateSpans.length === 0) {
@@ -16,7 +17,16 @@ export function transformTemplateExpression(state: TransformState, node: ts.Temp
 	}
 
 	for (const templateSpan of node.templateSpans) {
-		expressions.push(transformExpression(state, templateSpan.expression));
+		if (isStringType(state, state.getType(templateSpan.expression))) {
+			expressions.push(transformExpression(state, templateSpan.expression));
+		} else {
+			expressions.push(
+				lua.create(lua.SyntaxKind.CallExpression, {
+					expression: lua.globals.tostring,
+					args: lua.list.make(transformExpression(state, templateSpan.expression)),
+				}),
+			);
+		}
 
 		if (templateSpan.literal.text.length > 0) {
 			expressions.push(lua.string(templateSpan.literal.text));
