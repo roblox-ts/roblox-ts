562d4b5a1915f290618c1840e4c78c4d03c506ac
Noah Crowley <noahwillcrow@gmail.com>
2020-05-05 01:40:52 -0400

    quick refactor for templateSpan.expression

--- src/TSTransformer/nodes/expressions/transformTemplateExpression.ts
+++ src/TSTransformer/nodes/expressions/transformTemplateExpression.ts
@@ -17,16 +17,14 @@ export function transformTemplateExpression(state: TransformState, node: ts.Temp
 	}
 
 	for (const templateSpan of node.templateSpans) {
-		if (isStringType(state, state.getType(templateSpan.expression))) {
-			expressions.push(transformExpression(state, templateSpan.expression));
-		} else {
-			expressions.push(
-				lua.create(lua.SyntaxKind.CallExpression, {
-					expression: lua.globals.tostring,
-					args: lua.list.make(transformExpression(state, templateSpan.expression)),
-				}),
-			);
+		let exp = transformExpression(state, templateSpan.expression);
+		if (!isStringType(state, state.getType(templateSpan.expression))) {
+			exp = lua.create(lua.SyntaxKind.CallExpression, {
+				expression: lua.globals.tostring,
+				args: lua.list.make(exp),
+			});
 		}
+		expressions.push(exp);
 
 		if (templateSpan.literal.text.length > 0) {
 			expressions.push(lua.string(templateSpan.literal.text));
