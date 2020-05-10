32740640b71cb05540c5db7f19c5205b82852971
osyrisrblx <osyrisrblx@gmail.com>
2020-05-10 00:28:01 -0700

    Add spread type asserts

--- src/TSTransformer/nodes/expressions/transformSpreadElement.ts
+++ src/TSTransformer/nodes/expressions/transformSpreadElement.ts
@@ -4,12 +4,15 @@ import { assert } from "Shared/util/assert";
 import { diagnostics } from "TSTransformer/diagnostics";
 import { transformExpression } from "TSTransformer/nodes/expressions/transformExpression";
 import { TransformState } from "TSTransformer/TransformState";
+import { isArrayType } from "TSTransformer/util/types";
 
 export function transformSpreadElement(state: TransformState, node: ts.SpreadElement) {
 	assert(ts.isCallExpression(node.parent));
 	if (node.parent.arguments[node.parent.arguments.length - 1] !== node) {
 		state.addDiagnostic(diagnostics.noPrecedingSpreadElement(node));
 	}
+
+	assert(isArrayType(state, state.getType(node.expression)));
 
 	return lua.create(lua.SyntaxKind.CallExpression, {
 		expression: lua.globals.unpack,
