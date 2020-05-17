6312188e71961abffcc494c37d0c9837b3d0fca5
osyrisrblx <osyrisrblx@gmail.com>
2020-05-17 01:21:27 -0700

    Add Promise -> TS.Promise macro

--- src/TSTransformer/macros/identifierMacros.ts
+++ src/TSTransformer/macros/identifierMacros.ts
@@ -3,4 +3,5 @@ import { IdentifierMacro, MacroList } from "TSTransformer/macros/types";
 
 export const IDENTIFIER_MACROS: MacroList<IdentifierMacro> = {
 	PKG_VERSION: state => lua.string(state.compileState.pkgVersion ?? "0.0.0"),
+	Promise: state => state.TS("Promise"),
 };
