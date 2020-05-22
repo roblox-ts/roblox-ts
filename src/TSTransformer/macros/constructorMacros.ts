dfc174c989a52227dbf441c18e618c957ad5ff72
osyrisrblx <osyrisrblx@gmail.com>
2020-05-21 20:02:27 -0700

    Add constructor macros for ReadonlySet, ReadonlyMap

--- src/TSTransformer/macros/constructorMacros.ts
+++ src/TSTransformer/macros/constructorMacros.ts
@@ -24,94 +24,99 @@ function isFlatMap(expression: lua.Expression): expression is lua.Array {
 	return false;
 }
 
-export const CONSTRUCTOR_MACROS: MacroList<ConstructorMacro> = {
-	ArrayConstructor: (state, node) => {
-		if (node.arguments && node.arguments.length > 0) {
-			const args = ensureTransformOrder(state, node.arguments);
-			return lua.create(lua.SyntaxKind.CallExpression, {
-				expression: lua.globals.table.create,
-				args: lua.list.make(...args),
-			});
-		}
-		return lua.array();
-	},
+const ArrayConstructor: ConstructorMacro = (state, node) => {
+	if (node.arguments && node.arguments.length > 0) {
+		const args = ensureTransformOrder(state, node.arguments);
+		return lua.create(lua.SyntaxKind.CallExpression, {
+			expression: lua.globals.table.create,
+			args: lua.list.make(...args),
+		});
+	}
+	return lua.array();
+};
 
-	SetConstructor: (state, node) => {
-		if (!node.arguments || node.arguments.length === 0) {
-			return lua.set();
-		}
-		const arg = node.arguments[0];
-		if (ts.isArrayLiteralExpression(arg)) {
-			return lua.set(ensureTransformOrder(state, arg.elements));
-		} else {
-			const id = state.pushToVar(lua.set());
-			const valueId = lua.tempId();
-			state.prereq(
-				lua.create(lua.SyntaxKind.ForStatement, {
-					ids: lua.list.make<lua.AnyIdentifier>(lua.emptyId(), valueId),
-					expression: lua.create(lua.SyntaxKind.CallExpression, {
-						expression: lua.globals.ipairs,
-						args: lua.list.make(transformExpression(state, arg)),
-					}),
-					statements: lua.list.make(
-						lua.create(lua.SyntaxKind.Assignment, {
-							left: lua.create(lua.SyntaxKind.ComputedIndexExpression, {
-								expression: id,
-								index: valueId,
-							}),
-							right: lua.bool(true),
-						}),
-					),
+const SetConstructor: ConstructorMacro = (state, node) => {
+	if (!node.arguments || node.arguments.length === 0) {
+		return lua.set();
+	}
+	const arg = node.arguments[0];
+	if (ts.isArrayLiteralExpression(arg)) {
+		return lua.set(ensureTransformOrder(state, arg.elements));
+	} else {
+		const id = state.pushToVar(lua.set());
+		const valueId = lua.tempId();
+		state.prereq(
+			lua.create(lua.SyntaxKind.ForStatement, {
+				ids: lua.list.make<lua.AnyIdentifier>(lua.emptyId(), valueId),
+				expression: lua.create(lua.SyntaxKind.CallExpression, {
+					expression: lua.globals.ipairs,
+					args: lua.list.make(transformExpression(state, arg)),
 				}),
-			);
-			return id;
-		}
-	},
-
-	MapConstructor: (state, node) => {
-		if (!node.arguments || node.arguments.length === 0) {
-			return lua.map();
-		}
-		const arg = node.arguments[0];
-		const transformed = transformExpression(state, arg);
-		if (isFlatMap(transformed)) {
-			// TODO make this nicer?
-			const elements = lua.list.toArray(transformed.members).map(element => {
-				const e = element as lua.Array;
-				return [e.members.head?.value, e.members.head?.next?.value] as [lua.Expression, lua.Expression];
-			});
-			return lua.map(elements);
-		} else {
-			const id = state.pushToVar(lua.set());
-			const valueId = lua.id("value");
-			state.prereq(
-				lua.create(lua.SyntaxKind.ForStatement, {
-					ids: lua.list.make<lua.AnyIdentifier>(lua.emptyId(), valueId),
-					expression: lua.create(lua.SyntaxKind.CallExpression, {
-						expression: lua.globals.ipairs,
-						args: lua.list.make(transformed),
+				statements: lua.list.make(
+					lua.create(lua.SyntaxKind.Assignment, {
+						left: lua.create(lua.SyntaxKind.ComputedIndexExpression, {
+							expression: id,
+							index: valueId,
+						}),
+						right: lua.bool(true),
 					}),
-					statements: lua.list.make(
-						lua.create(lua.SyntaxKind.Assignment, {
-							left: lua.create(lua.SyntaxKind.ComputedIndexExpression, {
-								expression: id,
-								index: lua.create(lua.SyntaxKind.ComputedIndexExpression, {
-									expression: valueId,
-									index: lua.number(1),
-								}),
-							}),
-							right: lua.create(lua.SyntaxKind.ComputedIndexExpression, {
+				),
+			}),
+		);
+		return id;
+	}
+};
+
+const MapConstructor: ConstructorMacro = (state, node) => {
+	if (!node.arguments || node.arguments.length === 0) {
+		return lua.map();
+	}
+	const arg = node.arguments[0];
+	const transformed = transformExpression(state, arg);
+	if (isFlatMap(transformed)) {
+		// TODO make this nicer?
+		const elements = lua.list.toArray(transformed.members).map(element => {
+			const e = element as lua.Array;
+			return [e.members.head?.value, e.members.head?.next?.value] as [lua.Expression, lua.Expression];
+		});
+		return lua.map(elements);
+	} else {
+		const id = state.pushToVar(lua.set());
+		const valueId = lua.id("value");
+		state.prereq(
+			lua.create(lua.SyntaxKind.ForStatement, {
+				ids: lua.list.make<lua.AnyIdentifier>(lua.emptyId(), valueId),
+				expression: lua.create(lua.SyntaxKind.CallExpression, {
+					expression: lua.globals.ipairs,
+					args: lua.list.make(transformed),
+				}),
+				statements: lua.list.make(
+					lua.create(lua.SyntaxKind.Assignment, {
+						left: lua.create(lua.SyntaxKind.ComputedIndexExpression, {
+							expression: id,
+							index: lua.create(lua.SyntaxKind.ComputedIndexExpression, {
 								expression: valueId,
-								index: lua.number(2),
+								index: lua.number(1),
 							}),
 						}),
-					),
-				}),
-			);
-			return id;
-		}
-	},
+						right: lua.create(lua.SyntaxKind.ComputedIndexExpression, {
+							expression: valueId,
+							index: lua.number(2),
+						}),
+					}),
+				),
+			}),
+		);
+		return id;
+	}
+};
 
-	WeakSetConstructor: (state, node) => wrapWeak(state, node, CONSTRUCTOR_MACROS.SetConstructor),
-	WeakMapConstructor: (state, node) => wrapWeak(state, node, CONSTRUCTOR_MACROS.MapConstructor),
+export const CONSTRUCTOR_MACROS: MacroList<ConstructorMacro> = {
+	ArrayConstructor,
+	SetConstructor,
+	MapConstructor,
+	WeakSetConstructor: (state, node) => wrapWeak(state, node, SetConstructor),
+	WeakMapConstructor: (state, node) => wrapWeak(state, node, MapConstructor),
+	ReadonlyMapConstructor: MapConstructor,
+	ReadonlySetConstructor: MapConstructor,
 };
