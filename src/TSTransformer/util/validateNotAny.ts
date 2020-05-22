3dea2e3b20345a87c9afd8bc5fa362503dd1526b
osyrisrblx <osyrisrblx@gmail.com>
2020-05-21 17:14:21 -0700

    fix validateNotAny for spread elements

--- src/TSTransformer/util/validateNotAny.ts
+++ src/TSTransformer/util/validateNotAny.ts
@@ -1,12 +1,18 @@
 import ts from "byots";
-import { TransformState } from "TSTransformer";
 import { diagnostics } from "Shared/diagnostics";
-import { isAnyType, isArrayType, getTypeArguments } from "TSTransformer/util/types";
+import { TransformState } from "TSTransformer";
+import { skipDownwards } from "TSTransformer/util/traversal";
+import { getTypeArguments, isAnyType, isArrayType } from "TSTransformer/util/types";
 
 export function validateNotAnyType(state: TransformState, node: ts.Node) {
+	if (ts.isSpreadElement(node)) {
+		node = skipDownwards(node.expression);
+	}
+
 	let type = state.getType(node);
 
 	if (isArrayType(state, type)) {
+		// Array<T> -> T
 		const typeArguments = getTypeArguments(state, type);
 		if (typeArguments.length > 0) {
 			type = typeArguments[0];
