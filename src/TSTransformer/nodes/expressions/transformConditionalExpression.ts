ffc29938a56e5f3f53c5afd95ddc6d115c84af94
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2020-05-23 15:07:04 +0200

    [draft] simplify conditionalExpression
    makes it use "condition and trueValue or falseValue", where available. Needs implementation of `canBeFalsy`, but I'm not familiar with types.

--- src/TSTransformer/nodes/expressions/transformConditionalExpression.ts
+++ src/TSTransformer/nodes/expressions/transformConditionalExpression.ts
@@ -1,12 +1,35 @@
 import ts from "byots";
+import * as tsst from "ts-simple-type";
 import * as lua from "LuaAST";
 import { TransformState } from "TSTransformer";
 import { transformExpression } from "TSTransformer/nodes/expressions/transformExpression";
 import { createTruthinessChecks } from "TSTransformer/util/createTruthinessChecks";
 
-export function transformConditionalExpression(state: TransformState, node: ts.ConditionalExpression) {
-	const tempId = lua.tempId();
+function isBooleanType(type: tsst.SimpleType) {
+	return type.kind === tsst.SimpleTypeKind.BOOLEAN || type.kind === tsst.SimpleTypeKind.BOOLEAN_LITERAL;
+}
 
+declare function canBeFalsy(node: ts.Expression): boolean;
+export function transformConditionalExpression(state: TransformState, node: ts.ConditionalExpression) {
+	if (isBooleanType(state.getSimpleTypeFromNode(node)) && !canBeFalsy(node.whenTrue)) {
+		return lua.create(lua.SyntaxKind.ParenthesizedExpression, {
+			expression: lua.create(lua.SyntaxKind.BinaryExpression, {
+				left: lua.create(lua.SyntaxKind.BinaryExpression, {
+					left: createTruthinessChecks(
+						state,
+						transformExpression(state, node.condition),
+						state.getType(node.condition),
+					),
+					right: transformExpression(state, node.whenTrue),
+					operator: "and",
+				}),
+				right: transformExpression(state, node.whenFalse),
+				operator: "or",
+			}),
+		});
+	}
+	
+	const tempId = lua.tempId();
 	state.prereq(
 		lua.create(lua.SyntaxKind.VariableDeclaration, {
 			left: tempId,
