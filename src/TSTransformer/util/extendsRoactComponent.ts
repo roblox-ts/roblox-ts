459d9830eea898312f201783c5febbcae91c02ed
osyrisrblx <osyrisrblx@gmail.com>
2020-05-29 01:29:53 -0700

    fix RoactSymbolManager being non-optional

--- src/TSTransformer/util/extendsRoactComponent.ts
+++ src/TSTransformer/util/extendsRoactComponent.ts
@@ -4,15 +4,17 @@ import { ROACT_SYMBOL_NAMES } from "TSTransformer/classes/RoactSymbolManager";
 import { getExtendsNode } from "TSTransformer/util/getExtendsNode";
 
 export function extendsRoactComponent(state: TransformState, node: ts.ClassLikeDeclaration) {
-	const extendsNode = getExtendsNode(node);
-	if (extendsNode) {
-		const aliasSymbol = state.typeChecker.getSymbolAtLocation(extendsNode.expression);
-		if (aliasSymbol) {
-			const originalSymbol = ts.skipAlias(aliasSymbol, state.typeChecker);
-			return (
-				originalSymbol === state.roactSymbolManager.getSymbolOrThrow(ROACT_SYMBOL_NAMES.Component) ||
-				originalSymbol === state.roactSymbolManager.getSymbolOrThrow(ROACT_SYMBOL_NAMES.PureComponent)
-			);
+	if (state.roactSymbolManager) {
+		const extendsNode = getExtendsNode(node);
+		if (extendsNode) {
+			const aliasSymbol = state.typeChecker.getSymbolAtLocation(extendsNode.expression);
+			if (aliasSymbol) {
+				const originalSymbol = ts.skipAlias(aliasSymbol, state.typeChecker);
+				return (
+					originalSymbol === state.roactSymbolManager.getSymbolOrThrow(ROACT_SYMBOL_NAMES.Component) ||
+					originalSymbol === state.roactSymbolManager.getSymbolOrThrow(ROACT_SYMBOL_NAMES.PureComponent)
+				);
+			}
 		}
 	}
 	return false;
