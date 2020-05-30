35b96ade642f573810a31d149b479d9d094d5f92
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2020-05-30 20:10:17 +0200

    Add UNIQUE_GUID identifier macro commented because not in types

--- src/TSTransformer/macros/identifierMacros.ts
+++ src/TSTransformer/macros/identifierMacros.ts
@@ -1,7 +1,9 @@
 import * as lua from "LuaAST";
 import { IdentifierMacro, MacroList } from "TSTransformer/macros/types";
+import { v4 as uuidv4 } from "uuid";
 
 export const IDENTIFIER_MACROS: MacroList<IdentifierMacro> = {
 	PKG_VERSION: state => lua.string(state.compileState.pkgVersion ?? "0.0.0"),
+	// UNIQUE_GUID: () => lua.string(uuidv4()),
 	Promise: state => state.TS("Promise"),
 };
