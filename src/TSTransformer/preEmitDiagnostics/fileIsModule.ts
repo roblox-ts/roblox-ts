86386d9d3e7e4dcf983b0448ede97fcdd919b0ac
osyrisrblx <osyrisrblx@gmail.com>
2020-06-03 21:48:58 -0700

    Fix fileIsModule check

--- src/TSTransformer/preEmitDiagnostics/fileIsModule.ts
+++ src/TSTransformer/preEmitDiagnostics/fileIsModule.ts
@@ -2,18 +2,7 @@ import ts from "byots";
 import { diagnostics } from "Shared/diagnostics";
 
 export function fileIsModule(sourceFile: ts.SourceFile) {
-	if (
-		!sourceFile.statements.some(
-			node =>
-				// TODO make list complete
-				// missing all cases of `export const ...`
-				// but catches all cases of `export {}` and `import {}`
-				ts.isExportDeclaration(node) ||
-				ts.isExportAssignment(node) ||
-				ts.isImportCall(node) ||
-				ts.isImportDeclaration(node),
-		)
-	) {
+	if (sourceFile.externalModuleIndicator === undefined) {
 		return [diagnostics.missingImportOrExport(sourceFile.statements[0])];
 	}
 	return [];
