5f70cb2ec77f7581cfd9c9eae61e677357191302
osyrisrblx <osyrisrblx@gmail.com>
2020-06-08 21:16:58 -0700

    attempt to fix getFirstConstructSymbol

--- src/TSTransformer/util/getFirstConstructSymbol.ts
+++ src/TSTransformer/util/getFirstConstructSymbol.ts
@@ -4,7 +4,7 @@ import { TransformState } from "TSTransformer";
 export function getFirstConstructSymbol(state: TransformState, expression: ts.Expression) {
 	const type = state.getType(expression);
 	if (type.symbol) {
-		for (const declaration of type.symbol.declarations) {
+		for (const declaration of type.symbol.declarations ?? []) {
 			if (ts.isInterfaceDeclaration(declaration)) {
 				for (const member of declaration.members) {
 					if (ts.isConstructSignatureDeclaration(member)) {
