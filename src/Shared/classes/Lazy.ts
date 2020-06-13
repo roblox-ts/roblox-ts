56ecdb8a3a52a5ca4b6bbf04699e4dbc6fc0e412
osyrisrblx <osyrisrblx@gmail.com>
2020-06-13 11:42:10 -0700

    Add Lazy.ts

---
+++ src/Shared/classes/Lazy.ts
@@ -0,0 +1,14 @@
+export class Lazy<T> {
+	private isInitialized = false;
+	private value: T | undefined;
+
+	constructor(private readonly getValue: () => T) {}
+
+	public get() {
+		if (!this.isInitialized) {
+			this.isInitialized = true;
+			this.value = this.getValue();
+		}
+		return this.value as T;
+	}
+}
