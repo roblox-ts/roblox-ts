069818d6c4b307f185fd6124bfc6eee716ae3d65
osyrisrblx <osyrisrblx@gmail.com>
2020-07-20 15:26:10 -0700

    update package scripts

--- tests/src/tests/math.spec.ts
+++ tests/src/tests/math.spec.ts
@@ -1,69 +1,73 @@
 /// <reference types="@rbxts/testez/globals" />
 
 export = () => {
-	it("should add numbers", () => {
-		expect(1 + 5).to.equal(6);
-		expect(22 + 44).to.equal(66);
+	describe("should support basic math operators", () => {
+		it("should add numbers", () => {
+			expect(1 + 5).to.equal(6);
+			expect(22 + 44).to.equal(66);
+		});
+
+		it("should subtract numbers", () => {
+			expect(10 - 3).to.equal(7);
+			expect(20 - 37).to.equal(-17);
+		});
+
+		it("should multiply numbers", () => {
+			expect(5 * 7).to.equal(35);
+			expect(8 * 12).to.equal(96);
+		});
+
+		it("should divide numbers", () => {
+			expect(6 / 3).to.equal(2);
+			expect(22 / 44).to.equal(0.5);
+		});
+
+		it("should modulus numbers", () => {
+			expect(5 % 2).to.equal(1);
+			expect(100 % 17).to.equal(15);
+		});
+
+		it("should exponent numbers", () => {
+			expect(2 ** 3).to.equal(8);
+			expect(4 ** 5).to.equal(1024);
+		});
 	});
 
-	it("should subtract numbers", () => {
-		expect(10 - 3).to.equal(7);
-		expect(20 - 37).to.equal(-17);
+	describe("should support math unary operators", () => {
+		it("should post increment", () => {
+			let x = 10;
+			x++;
+			expect(x).to.equal(11);
+			expect(x++).to.equal(11);
+			expect(x).to.equal(12);
+		});
+
+		it("should pre increment", () => {
+			let x = 10;
+			++x;
+			expect(x).to.equal(11);
+			expect(++x).to.equal(12);
+			expect(x).to.equal(12);
+		});
+
+		it("should post decrement", () => {
+			let x = 10;
+			x--;
+			expect(x).to.equal(9);
+			expect(x--).to.equal(9);
+			expect(x).to.equal(8);
+		});
+
+		it("should pre decrement", () => {
+			let x = 10;
+			--x;
+			expect(x).to.equal(9);
+			expect(--x).to.equal(8);
+			expect(x).to.equal(8);
+		});
 	});
 
-	it("should multiply numbers", () => {
-		expect(5 * 7).to.equal(35);
-		expect(8 * 12).to.equal(96);
-	});
-
-	it("should divide numbers", () => {
-		expect(6 / 3).to.equal(2);
-		expect(22 / 44).to.equal(0.5);
-	});
-
-	it("should modulus numbers", () => {
-		expect(5 % 2).to.equal(1);
-		expect(100 % 17).to.equal(15);
-	});
-
-	it("should exponent numbers", () => {
-		expect(2 ** 3).to.equal(8);
-		expect(4 ** 5).to.equal(1024);
-	});
-
-	it("should post increment", () => {
-		let x = 10;
-		x++;
-		expect(x).to.equal(11);
-		expect(x++).to.equal(11);
-		expect(x).to.equal(12);
-	});
-
-	it("should pre increment", () => {
-		let x = 10;
-		++x;
-		expect(x).to.equal(11);
-		expect(++x).to.equal(12);
-		expect(x).to.equal(12);
-	});
-
-	it("should post decrement", () => {
-		let x = 10;
-		x--;
-		expect(x).to.equal(9);
-		expect(x--).to.equal(9);
-		expect(x).to.equal(8);
-	});
-
-	it("should pre decrement", () => {
-		let x = 10;
-		--x;
-		expect(x).to.equal(9);
-		expect(--x).to.equal(8);
-		expect(x).to.equal(8);
-	});
-
-	describe("should compound assign", () => {
+	describe("should support compound assignment", () => {
 		it("should compound assign addition", () => {
 			let x = 10;
 			x += 2;
@@ -134,21 +138,5 @@ export = () => {
 			expect(c.X).to.equal(4);
 			expect(c.Y).to.equal(6);
 		});
-	});
-
-	it("should add computedAccessExpressions with numbers", () => {
-		const foo = {
-			a: 1,
-			b: 2,
-			c: 3,
-		};
-
-		function f<T extends keyof typeof foo>(k: T) {
-			return 5 + foo[k];
-		}
-
-		expect(f("a")).to.equal(6);
-		expect(f("b")).to.equal(7);
-		expect(f("c")).to.equal(8);
 	});
 };
