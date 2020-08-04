4efe8f71937533d29eb98f4e96953b26c0804994
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2020-08-04 14:26:44 +0200

    change `result` to `kind` in getKindName

--- src/TSTransformer/util/getKindName.ts
+++ src/TSTransformer/util/getKindName.ts
@@ -1,30 +1,30 @@
 import ts from "byots";
 
-export function getKindName(result: ts.SyntaxKind) {
-	if (result === ts.SyntaxKind.FirstAssignment) return "EqualsToken";
-	if (result === ts.SyntaxKind.FirstCompoundAssignment) return "PlusEqualsToken";
-	if (result === ts.SyntaxKind.LastReservedWord) return "WithKeyword";
-	if (result === ts.SyntaxKind.FirstKeyword) return "BreakKeyword";
-	if (result === ts.SyntaxKind.FirstFutureReservedWord) return "ImplementsKeyword";
-	if (result === ts.SyntaxKind.LastFutureReservedWord) return "YieldKeyword";
-	if (result === ts.SyntaxKind.FirstTypeNode) return "TypePredicate";
-	if (result === ts.SyntaxKind.LastTypeNode) return "ImportType";
-	if (result === ts.SyntaxKind.FirstPunctuation) return "OpenBraceToken";
-	if (result === ts.SyntaxKind.FirstToken) return "Unknown";
-	if (result === ts.SyntaxKind.FirstTriviaToken) return "SingleLineCommentTrivia";
-	if (result === ts.SyntaxKind.LastTriviaToken) return "ConflictMarkerTrivia";
-	if (result === ts.SyntaxKind.FirstLiteralToken) return "NumericLiteral";
-	if (result === ts.SyntaxKind.FirstTemplateToken) return "NoSubstitutionTemplateLiteral";
-	if (result === ts.SyntaxKind.LastTemplateToken) return "TemplateTail";
-	if (result === ts.SyntaxKind.FirstBinaryOperator) return "LessThanToken";
-	if (result === ts.SyntaxKind.LastBinaryOperator) return "CaretEqualsToken";
-	if (result === ts.SyntaxKind.FirstStatement) return "VariableStatement";
-	if (result === ts.SyntaxKind.LastStatement) return "DebuggerStatement";
-	if (result === ts.SyntaxKind.FirstNode) return "QualifiedName";
-	if (result === ts.SyntaxKind.FirstJSDocNode) return "JSDocTypeExpression";
-	if (result === ts.SyntaxKind.FirstJSDocTagNode) return "JSDocTag";
-	if (result === ts.SyntaxKind.LastJSDocTagNode) return "JSDocPropertyTag";
-	if (result === ts.SyntaxKind.FirstContextualKeyword) return "AbstractKeyword";
-	if (result === ts.SyntaxKind.LastContextualKeyword) return "OfKeyword";
-	return ts.SyntaxKind[result];
+export function getKindName(kind: ts.SyntaxKind) {
+	if (kind === ts.SyntaxKind.FirstAssignment) return "EqualsToken";
+	if (kind === ts.SyntaxKind.FirstCompoundAssignment) return "PlusEqualsToken";
+	if (kind === ts.SyntaxKind.LastReservedWord) return "WithKeyword";
+	if (kind === ts.SyntaxKind.FirstKeyword) return "BreakKeyword";
+	if (kind === ts.SyntaxKind.FirstFutureReservedWord) return "ImplementsKeyword";
+	if (kind === ts.SyntaxKind.LastFutureReservedWord) return "YieldKeyword";
+	if (kind === ts.SyntaxKind.FirstTypeNode) return "TypePredicate";
+	if (kind === ts.SyntaxKind.LastTypeNode) return "ImportType";
+	if (kind === ts.SyntaxKind.FirstPunctuation) return "OpenBraceToken";
+	if (kind === ts.SyntaxKind.FirstToken) return "Unknown";
+	if (kind === ts.SyntaxKind.FirstTriviaToken) return "SingleLineCommentTrivia";
+	if (kind === ts.SyntaxKind.LastTriviaToken) return "ConflictMarkerTrivia";
+	if (kind === ts.SyntaxKind.FirstLiteralToken) return "NumericLiteral";
+	if (kind === ts.SyntaxKind.FirstTemplateToken) return "NoSubstitutionTemplateLiteral";
+	if (kind === ts.SyntaxKind.LastTemplateToken) return "TemplateTail";
+	if (kind === ts.SyntaxKind.FirstBinaryOperator) return "LessThanToken";
+	if (kind === ts.SyntaxKind.LastBinaryOperator) return "CaretEqualsToken";
+	if (kind === ts.SyntaxKind.FirstStatement) return "VariableStatement";
+	if (kind === ts.SyntaxKind.LastStatement) return "DebuggerStatement";
+	if (kind === ts.SyntaxKind.FirstNode) return "QualifiedName";
+	if (kind === ts.SyntaxKind.FirstJSDocNode) return "JSDocTypeExpression";
+	if (kind === ts.SyntaxKind.FirstJSDocTagNode) return "JSDocTag";
+	if (kind === ts.SyntaxKind.LastJSDocTagNode) return "JSDocPropertyTag";
+	if (kind === ts.SyntaxKind.FirstContextualKeyword) return "AbstractKeyword";
+	if (kind === ts.SyntaxKind.LastContextualKeyword) return "OfKeyword";
+	return ts.SyntaxKind[kind];
 }
