ba20fdd398d60f61898b849a9a6aee9931e19f43
osyrisrblx <osyrisrblx@gmail.com>
2020-08-24 01:34:42 -0700

    revert transformExpression, transformStatement

--- src/TSTransformer/nodes/expressions/transformExpression.ts
+++ src/TSTransformer/nodes/expressions/transformExpression.ts
@@ -7,6 +7,7 @@ import { transformArrayLiteralExpression } from "TSTransformer/nodes/expressions
 import { transformAsExpression } from "TSTransformer/nodes/expressions/transformAsExpression";
 import { transformAwaitExpression } from "TSTransformer/nodes/expressions/transformAwaitExpression";
 import { transformBinaryExpression } from "TSTransformer/nodes/expressions/transformBinaryExpression";
+import { transformFalseKeyword, transformTrueKeyword } from "TSTransformer/nodes/expressions/transformBooleanLiteral";
 import { transformCallExpression } from "TSTransformer/nodes/expressions/transformCallExpression";
 import { transformClassExpression } from "TSTransformer/nodes/expressions/transformClassExpression";
 import { transformConditionalExpression } from "TSTransformer/nodes/expressions/transformConditionalExpression";
@@ -15,20 +16,17 @@ import { transformFunctionExpression } from "TSTransformer/nodes/expressions/tra
 import { transformIdentifier } from "TSTransformer/nodes/expressions/transformIdentifier";
 import { transformJsxElement } from "TSTransformer/nodes/expressions/transformJsxElement";
 import { transformJsxSelfClosingElement } from "TSTransformer/nodes/expressions/transformJsxSelfClosingElement";
-import { transformFalseKeyword, transformTrueKeyword } from "TSTransformer/nodes/expressions/transformBooleanLiteral";
-import { transformStringLiteral } from "TSTransformer/nodes/expressions/transformStringLiteral";
-import { transformNumericLiteral } from "TSTransformer/nodes/expressions/transformNumericLiteral";
 import { transformNewExpression } from "TSTransformer/nodes/expressions/transformNewExpression";
 import { transformNonNullExpression } from "TSTransformer/nodes/expressions/transformNonNullExpression";
+import { transformNumericLiteral } from "TSTransformer/nodes/expressions/transformNumericLiteral";
 import { transformObjectLiteralExpression } from "TSTransformer/nodes/expressions/transformObjectLiteralExpression";
 import { transformParenthesizedExpression } from "TSTransformer/nodes/expressions/transformParenthesizedExpression";
 import { transformPropertyAccessExpression } from "TSTransformer/nodes/expressions/transformPropertyAccessExpression";
 import { transformSpreadElement } from "TSTransformer/nodes/expressions/transformSpreadElement";
-import { transformSuperExpression } from "TSTransformer/nodes/expressions/transformSuperExpression";
+import { transformStringLiteral } from "TSTransformer/nodes/expressions/transformStringLiteral";
 import { transformTaggedTemplateExpression } from "TSTransformer/nodes/expressions/transformTaggedTemplateExpression";
 import { transformTemplateExpression } from "TSTransformer/nodes/expressions/transformTemplateExpression";
 import { transformThisExpression } from "TSTransformer/nodes/expressions/transformThisExpression";
-import { transformTypeAssertionExpression } from "TSTransformer/nodes/expressions/transformTypeAssertionExpression";
 import {
 	transformPostfixUnaryExpression,
 	transformPrefixUnaryExpression,
@@ -44,51 +42,49 @@ const DIAGNOSTIC = (factory: DiagnosticFactory) => (state: TransformState, node:
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type ExpressionTransformer = (state: TransformState, node: any) => luau.Expression;
 
-const TRANSFORMER_BY_KIND: Partial<Record<ts.Expression["kind"], ExpressionTransformer>> = {
+const TRANSFORMER_BY_KIND = new Map<ts.SyntaxKind, ExpressionTransformer>([
 	// banned expressions
-	[ts.SyntaxKind.DeleteExpression]: DIAGNOSTIC(diagnostics.noDeleteExpression),
-	[ts.SyntaxKind.NullKeyword]: DIAGNOSTIC(diagnostics.noNullLiteral),
-	[ts.SyntaxKind.PrivateIdentifier]: DIAGNOSTIC(diagnostics.noPrivateIdentifier),
-	[ts.SyntaxKind.TypeOfExpression]: DIAGNOSTIC(diagnostics.noTypeOfExpression),
-	[ts.SyntaxKind.RegularExpressionLiteral]: DIAGNOSTIC(diagnostics.noRegex),
+	[ts.SyntaxKind.DeleteExpression, DIAGNOSTIC(diagnostics.noDeleteExpression)],
+	[ts.SyntaxKind.NullKeyword, DIAGNOSTIC(diagnostics.noNullLiteral)],
+	[ts.SyntaxKind.PrivateIdentifier, DIAGNOSTIC(diagnostics.noPrivateIdentifier)],
+	[ts.SyntaxKind.TypeOfExpression, DIAGNOSTIC(diagnostics.noTypeOfExpression)],
+	[ts.SyntaxKind.RegularExpressionLiteral, DIAGNOSTIC(diagnostics.noRegex)],
 
 	// regular transforms
-	[ts.SyntaxKind.ArrayLiteralExpression]: transformArrayLiteralExpression,
-	[ts.SyntaxKind.ArrowFunction]: transformFunctionExpression,
-	[ts.SyntaxKind.AsExpression]: transformAsExpression,
-	[ts.SyntaxKind.AwaitExpression]: transformAwaitExpression,
-	[ts.SyntaxKind.BinaryExpression]: transformBinaryExpression,
-	[ts.SyntaxKind.CallExpression]: transformCallExpression,
-	[ts.SyntaxKind.ClassExpression]: transformClassExpression,
-	[ts.SyntaxKind.ConditionalExpression]: transformConditionalExpression,
-	[ts.SyntaxKind.ElementAccessExpression]: transformElementAccessExpression,
-	[ts.SyntaxKind.FalseKeyword]: transformFalseKeyword,
-	[ts.SyntaxKind.FunctionExpression]: transformFunctionExpression,
-	[ts.SyntaxKind.Identifier]: transformIdentifier,
-	[ts.SyntaxKind.JsxElement]: transformJsxElement,
-	[ts.SyntaxKind.JsxSelfClosingElement]: transformJsxSelfClosingElement,
-	[ts.SyntaxKind.NewExpression]: transformNewExpression,
-	[ts.SyntaxKind.NonNullExpression]: transformNonNullExpression,
-	[ts.SyntaxKind.NoSubstitutionTemplateLiteral]: transformStringLiteral,
-	[ts.SyntaxKind.NumericLiteral]: transformNumericLiteral,
-	[ts.SyntaxKind.ObjectLiteralExpression]: transformObjectLiteralExpression,
-	[ts.SyntaxKind.ParenthesizedExpression]: transformParenthesizedExpression,
-	[ts.SyntaxKind.PostfixUnaryExpression]: transformPostfixUnaryExpression,
-	[ts.SyntaxKind.PrefixUnaryExpression]: transformPrefixUnaryExpression,
-	[ts.SyntaxKind.PropertyAccessExpression]: transformPropertyAccessExpression,
-	[ts.SyntaxKind.SpreadElement]: transformSpreadElement,
-	[ts.SyntaxKind.StringLiteral]: transformStringLiteral,
-	[ts.SyntaxKind.SuperKeyword]: transformSuperExpression,
-	[ts.SyntaxKind.TaggedTemplateExpression]: transformTaggedTemplateExpression,
-	[ts.SyntaxKind.TemplateExpression]: transformTemplateExpression,
-	[ts.SyntaxKind.ThisKeyword]: transformThisExpression,
-	[ts.SyntaxKind.TrueKeyword]: transformTrueKeyword,
-	[ts.SyntaxKind.VoidExpression]: transformVoidExpression,
-	[ts.SyntaxKind.TypeAssertionExpression]: transformTypeAssertionExpression,
-};
+	[ts.SyntaxKind.ArrayLiteralExpression, transformArrayLiteralExpression],
+	[ts.SyntaxKind.ArrowFunction, transformFunctionExpression],
+	[ts.SyntaxKind.AsExpression, transformAsExpression],
+	[ts.SyntaxKind.AwaitExpression, transformAwaitExpression],
+	[ts.SyntaxKind.BinaryExpression, transformBinaryExpression],
+	[ts.SyntaxKind.CallExpression, transformCallExpression],
+	[ts.SyntaxKind.ClassExpression, transformClassExpression],
+	[ts.SyntaxKind.ConditionalExpression, transformConditionalExpression],
+	[ts.SyntaxKind.ElementAccessExpression, transformElementAccessExpression],
+	[ts.SyntaxKind.FalseKeyword, transformFalseKeyword],
+	[ts.SyntaxKind.FunctionExpression, transformFunctionExpression],
+	[ts.SyntaxKind.Identifier, transformIdentifier],
+	[ts.SyntaxKind.JsxElement, transformJsxElement],
+	[ts.SyntaxKind.JsxSelfClosingElement, transformJsxSelfClosingElement],
+	[ts.SyntaxKind.NewExpression, transformNewExpression],
+	[ts.SyntaxKind.NonNullExpression, transformNonNullExpression],
+	[ts.SyntaxKind.NoSubstitutionTemplateLiteral, transformStringLiteral],
+	[ts.SyntaxKind.NumericLiteral, transformNumericLiteral],
+	[ts.SyntaxKind.ObjectLiteralExpression, transformObjectLiteralExpression],
+	[ts.SyntaxKind.ParenthesizedExpression, transformParenthesizedExpression],
+	[ts.SyntaxKind.PostfixUnaryExpression, transformPostfixUnaryExpression],
+	[ts.SyntaxKind.PrefixUnaryExpression, transformPrefixUnaryExpression],
+	[ts.SyntaxKind.PropertyAccessExpression, transformPropertyAccessExpression],
+	[ts.SyntaxKind.SpreadElement, transformSpreadElement],
+	[ts.SyntaxKind.StringLiteral, transformStringLiteral],
+	[ts.SyntaxKind.TaggedTemplateExpression, transformTaggedTemplateExpression],
+	[ts.SyntaxKind.TemplateExpression, transformTemplateExpression],
+	[ts.SyntaxKind.ThisKeyword, transformThisExpression],
+	[ts.SyntaxKind.TrueKeyword, transformTrueKeyword],
+	[ts.SyntaxKind.VoidExpression, transformVoidExpression],
+]);
 
 export function transformExpression(state: TransformState, node: ts.Expression): luau.Expression {
-	const transformer = TRANSFORMER_BY_KIND[node.kind];
+	const transformer = TRANSFORMER_BY_KIND.get(node.kind);
 	if (transformer) {
 		return transformer(state, node);
 	}
--- src/TSTransformer/nodes/expressions/transformSuperExpression.ts
+++
@@ -1,7 +0,0 @@
-import ts from "byots";
-import luau from "LuauAST";
-import { TransformState } from "TSTransformer";
-
-export function transformSuperExpression(state: TransformState, node: ts.SuperExpression) {
-	return luau.globals.super;
-}
