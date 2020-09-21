78f2bb0c02f3ba269bce982cc20f55e2be6a5cf0
osyrisrblx <osyrisrblx@gmail.com>
2020-09-20 21:17:01 -0700

    Allow decimal exponent literals to use +/- immediately after e

--- src/Shared/util/isValidLuauNumberLiteral.ts
+++ src/Shared/util/isValidLuauNumberLiteral.ts
@@ -1,4 +1,4 @@
-const DECIMAL_LITERAL_REGEX = /^(?:\d[\d_]*(?:\.[\d_]*)?|\.\d[\d_]*)(?:[eE]_*\d[\d_]*)?$/;
+const DECIMAL_LITERAL_REGEX = /^(?:\d[\d_]*(?:\.[\d_]*)?|\.\d[\d_]*)(?:[eE][\+\-]?_*\d[\d_]*)?$/;
 const BINARY_LITERAL_REGEX = /^0_*[bB]_*[01][01_]*$/;
 const HEXADECIMAL_LITERAL_REGEX = /^0_*[xX]_*[\da-fA-F][\da-fA-F_]*$/;
 
