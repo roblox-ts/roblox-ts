ada6469fda3fc7ab156bca79f1e0e0999c5a39a7
osyrisrblx <osyrisrblx@gmail.com>
2020-09-21 17:47:28 -0700

    Fix test error reporting

--- src/CLI/test.ts
+++ src/CLI/test.ts
@@ -2,6 +2,8 @@ import { describe } from "mocha";
 import path from "path";
 import { Project } from "Project";
 import { PACKAGE_ROOT } from "Shared/constants";
+import { DiagnosticError } from "Shared/errors/DiagnosticError";
+import { ProjectError } from "Shared/errors/ProjectError";
 
 describe("should compile tests project", () => {
 	const project = new Project(
@@ -16,9 +18,22 @@ describe("should compile tests project", () => {
 	);
 
 	it("should copy include files", () => project.copyInclude());
+
 	it("should copy non-compiled files", () => project.copyFiles(new Set(project.getRootDirs())));
+
 	for (const sourceFile of project.getChangedSourceFiles()) {
 		const fileName = path.relative(process.cwd(), sourceFile.fileName);
-		it(`should compile ${fileName}`, () => project.compileFiles([sourceFile]));
+		it(`should compile ${fileName}`, done => {
+			try {
+				project.compileFiles([sourceFile]);
+				done();
+			} catch (e) {
+				if (e instanceof ProjectError || e instanceof DiagnosticError) {
+					done(new Error("\n" + e.toString()));
+				} else {
+					done(e);
+				}
+			}
+		});
 	}
 });
