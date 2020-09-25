5f071b407b51731aba879e081dc04c9358b820cb
osyrisrblx <osyrisrblx@gmail.com>
2020-09-25 00:35:47 -0700

    Move createParseConfigFileHost into getParsedCommandLine

--- src/Project/functions/getParsedCommandLine.ts
+++ src/Project/functions/getParsedCommandLine.ts
@@ -1,9 +1,21 @@
 import ts from "byots";
 import { ProjectData } from "Project";
 import { validateCompilerOptions } from "Project/functions/validateCompilerOptions";
-import { createParseConfigFileHost } from "Project/util/createParseConfigFileHost";
 import { DiagnosticError } from "Shared/errors/DiagnosticError";
 import { ProjectError } from "Shared/errors/ProjectError";
+
+function createParseConfigFileHost(): ts.ParseConfigFileHost {
+	return {
+		fileExists: ts.sys.fileExists,
+		getCurrentDirectory: ts.sys.getCurrentDirectory,
+		onUnRecoverableConfigFileDiagnostic: d => {
+			throw new DiagnosticError([d]);
+		},
+		readDirectory: ts.sys.readDirectory,
+		readFile: ts.sys.readFile,
+		useCaseSensitiveFileNames: ts.sys.useCaseSensitiveFileNames,
+	};
+}
 
 export function getParsedCommandLine(data: ProjectData) {
 	const parsedCommandLine = ts.getParsedCommandLineOfConfigFile(data.tsConfigPath, {}, createParseConfigFileHost());
