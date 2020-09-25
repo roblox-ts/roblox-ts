3001b735f1ba3e764f087bdb7f55350007d5bcc6
osyrisrblx <osyrisrblx@gmail.com>
2020-09-25 00:37:10 -0700

    Move tryRemoveOutput into it's own file

--- src/Project/functions/cleanup.ts
+++ src/Project/functions/cleanup.ts
@@ -1,28 +1,7 @@
 import fs from "fs-extra";
 import path from "path";
-import { LogService } from "Shared/classes/LogService";
+import { tryRemoveOutput } from "Project/functions/tryRemoveOutput";
 import { PathTranslator } from "Shared/classes/PathTranslator";
-
-function isOutputFileOrphaned(pathTranslator: PathTranslator, filePath: string) {
-	for (const path of pathTranslator.getInputPaths(filePath)) {
-		if (fs.pathExistsSync(path)) {
-			return false;
-		}
-	}
-
-	if (pathTranslator.buildInfoOutputPath === filePath) {
-		return false;
-	}
-
-	return true;
-}
-
-export function tryRemoveOutput(pathTranslator: PathTranslator, outPath: string) {
-	if (isOutputFileOrphaned(pathTranslator, outPath)) {
-		fs.removeSync(outPath);
-		LogService.writeLineIfVerbose(`remove ${outPath}`);
-	}
-}
 
 function cleanupDirRecursively(pathTranslator: PathTranslator, dir: string) {
 	if (fs.pathExistsSync(dir)) {
