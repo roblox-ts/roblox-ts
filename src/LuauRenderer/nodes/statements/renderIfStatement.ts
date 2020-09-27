749a145759560c3291bcf7b7217e9b823c69b2be
osyrisrblx <osyrisrblx@gmail.com>
2020-09-27 04:21:08 -0700

    Remove "shorthand" if statement rendering

--- src/LuauRenderer/nodes/statements/renderIfStatement.ts
+++ src/LuauRenderer/nodes/statements/renderIfStatement.ts
@@ -2,32 +2,7 @@ import luau from "LuauAST";
 import { render, RenderState } from "LuauRenderer";
 import { renderStatements } from "LuauRenderer/util/renderStatements";
 
-function renderShorthandIfStatement(state: RenderState, node: luau.IfStatement) {
-	const statementStr = renderStatements(state, node.statements).trim();
-	return state.line(`if ${render(state, node.condition)} then ${statementStr} end`);
-}
-
-/** must be if X == nil then X = Y end */
-function shouldRenderShorthand(node: luau.IfStatement): boolean {
-	return (
-		luau.isBinaryExpression(node.condition) &&
-		luau.isAnyIdentifier(node.condition.left) &&
-		node.condition.operator === "==" &&
-		luau.isNilLiteral(node.condition.right) &&
-		luau.list.isList(node.elseBody) &&
-		node.elseBody.head === undefined &&
-		node.statements.head !== undefined &&
-		node.statements.head === node.statements.tail &&
-		luau.isAssignment(node.statements.head.value)
-	);
-}
-
 export function renderIfStatement(state: RenderState, node: luau.IfStatement) {
-	// if no elseBody, and only one statement
-	if (shouldRenderShorthand(node)) {
-		return renderShorthandIfStatement(state, node);
-	}
-
 	let result = "";
 
 	result += state.line(`if ${render(state, node.condition)} then`);
