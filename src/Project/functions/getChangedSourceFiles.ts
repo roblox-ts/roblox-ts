9351ceb638e6bf0a9f8e1d8439d2bfbf3d371424
osyrisrblx <osyrisrblx@gmail.com>
2020-10-05 15:05:34 -0700

    Fix #1134

--- src/Project/functions/getChangedSourceFiles.ts
+++ src/Project/functions/getChangedSourceFiles.ts
@@ -1,13 +1,11 @@
 import ts from "byots";
 import { getChangedFilePaths } from "Project/functions/getChangedFilePaths";
-import { assert } from "Shared/util/assert";
 
 export function getChangedSourceFiles(program: ts.BuilderProgram, pathHints?: Array<string>) {
 	const sourceFiles = new Array<ts.SourceFile>();
 	for (const fileName of getChangedFilePaths(program, pathHints)) {
 		const sourceFile = program.getSourceFile(fileName);
-		assert(sourceFile);
-		if (!sourceFile.isDeclarationFile && !ts.isJsonSourceFile(sourceFile)) {
+		if (sourceFile && !sourceFile.isDeclarationFile && !ts.isJsonSourceFile(sourceFile)) {
 			sourceFiles.push(sourceFile);
 		}
 	}
