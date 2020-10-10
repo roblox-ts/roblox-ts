db24d57779641c3652b2c3547a66ec79634e56ce
osyrisrblx <osyrisrblx@gmail.com>
2020-10-09 19:45:46 -0700

    Add 'debugger' to LoggableError constructor

--- src/Shared/errors/LoggableError.ts
+++ src/Shared/errors/LoggableError.ts
@@ -1,7 +1,12 @@
 import { LogService } from "Shared/classes/LogService";
 
 export abstract class LoggableError {
+	constructor() {
+		debugger;
+	}
+
 	public abstract toString(): string;
+
 	public log() {
 		LogService.writeLine(this.toString());
 	}
