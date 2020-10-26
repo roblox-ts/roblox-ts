b13517ae80b4b7fc1d41129608b0999f157665c0
osyrisrblx <osyrisrblx@gmail.com>
2020-10-26 15:01:55 -0700

    Fix linting issue

--- src/Project/functions/copyItem.ts
+++ src/Project/functions/copyItem.ts
@@ -5,7 +5,11 @@ import { isCompilableFile } from "Project/util/isCompilableFile";
 export function copyItem(services: ProjectServices, item: string) {
 	fs.copySync(item, services.pathTranslator.getOutputPath(item), {
 		filter: (src, dest) => {
-			if (fs.pathExistsSync(dest) && !fs.lstatSync(src).isDirectory() && fs.readFileSync(src).toString() === fs.readFileSync(dest).toString()) {
+			if (
+				fs.pathExistsSync(dest) &&
+				!fs.lstatSync(src).isDirectory() &&
+				fs.readFileSync(src).toString() === fs.readFileSync(dest).toString()
+			) {
 				return false;
 			}
 			return !isCompilableFile(src);
