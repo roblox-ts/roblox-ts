13579d0a377bba70c2fa38f6eaf69fabeecba120
liamlutton <lutton+gh@me.com>
2020-10-25 17:10:03 -0700

    Only copy item if file contents changed

--- src/Project/functions/copyItem.ts
+++ src/Project/functions/copyItem.ts
@@ -4,7 +4,12 @@ import { isCompilableFile } from "Project/util/isCompilableFile";
 
 export function copyItem(services: ProjectServices, item: string) {
 	fs.copySync(item, services.pathTranslator.getOutputPath(item), {
-		filter: src => !isCompilableFile(src),
+		filter: (src, dest) => {
+			if (fs.pathExistsSync(dest) && !fs.lstatSync(src).isDirectory() && fs.readFileSync(src).toString() === fs.readFileSync(dest).toString()) {
+				return false;
+			}
+			return !isCompilableFile(src);
+		},
 		dereference: true,
 	});
 }
