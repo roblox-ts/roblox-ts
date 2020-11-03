a284c8f2deb13f7316a0810053951c632126a4b6
osyrisrblx <osyrisrblx@gmail.com>
2020-11-03 14:53:14 -0800

    Update isBlockedByTryStatement.ts

--- src/TSTransformer/util/isBlockedByTryStatement.ts
+++ src/TSTransformer/util/isBlockedByTryStatement.ts
@@ -1,17 +1,18 @@
 import ts from "byots";
 
 export function isReturnBlockedByTryStatement(node: ts.Node) {
-	const ancestor = ts.findAncestor(node, ancestor => {
-		return ts.isTryStatement(ancestor) || ts.isFunctionLikeDeclaration(ancestor);
-	});
+	const ancestor = ts.findAncestor(
+		node,
+		ancestor => ts.isTryStatement(ancestor) || ts.isFunctionLikeDeclaration(ancestor),
+	);
 	return ancestor && ts.isTryStatement(ancestor);
 }
 
 export function isBreakBlockedByTryStatement(node: ts.Node) {
-	const ancestor = ts.findAncestor(node, ancestor => {
-		return (
-			ts.isTryStatement(ancestor) || ts.isIterationStatement(ancestor, false) || ts.isSwitchStatement(ancestor)
-		);
-	});
+	const ancestor = ts.findAncestor(
+		node,
+		ancestor =>
+			ts.isTryStatement(ancestor) || ts.isIterationStatement(ancestor, false) || ts.isSwitchStatement(ancestor),
+	);
 	return ancestor && ts.isTryStatement(ancestor);
 }
