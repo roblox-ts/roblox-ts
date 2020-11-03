abda2916c24f40d0ceb5bb7210be72571c63e141
osyrisrblx <osyrisrblx@gmail.com>
2020-11-03 14:53:18 -0800

    Update try.spec.ts

--- tests/src/tests/try.spec.ts
+++ tests/src/tests/try.spec.ts
@@ -163,4 +163,22 @@ export = () => {
 
 		expect(foo()).to.equal(2);
 	});
+
+	it("should support loops inside of try/catch with break", () => {
+		function foo() {
+			let x = 0;
+			try {
+				for (let i = 0; i < 10; i++) {
+					if (i === 5) {
+						return x;
+					} else if (i % 2 === 0) {
+						continue;
+					}
+					x++;
+				}
+			} catch {}
+		}
+
+		expect(foo()).to.equal(2);
+	});
 };
