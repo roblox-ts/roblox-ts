ec62af6e7befa04e315fb34bedbd58e1f746ec33
Osyris <osyrisrblx@gmail.com>
2020-11-04 10:38:42 -0800

    Improve Test Coverage (#1160)
    * Add assignment.spec.ts
    
    * Update function.spec.ts
    
    * Remove unused file
    
    * Add istanbul ignores
    
    * Remove istanbul ignore file

---
+++ tests/src/tests/assignment.spec.ts
@@ -0,0 +1,39 @@
+/// <reference types="@rbxts/testez/globals" />
+
+export = () => {
+	it("should support logical null coalescing assignment statement", () => {
+		let x: boolean | undefined;
+		x ??= true;
+		expect(x).to.equal(true);
+	});
+
+	it("should support logical or assignment statement", () => {
+		let x = false;
+		x ||= true;
+		expect(x).to.equal(true);
+	});
+
+	it("should support logical and assignment statement", () => {
+		let x = true;
+		x &&= false;
+		expect(x).to.equal(false);
+	});
+
+	it("should support logical null coalescing assignment expression", () => {
+		let x: boolean | undefined;
+		expect(x ??= true).to.equal(true);
+		expect(x).to.equal(true);
+	});
+
+	it("should support logical or assignment expression", () => {
+		let x = false;
+		expect(x ||= true).to.equal(true);
+		expect(x).to.equal(true);
+	});
+
+	it("should support logical and assignment expression", () => {
+		let x = true;
+		expect(x &&= false).to.equal(false);
+		expect(x).to.equal(false);
+	});
+};
