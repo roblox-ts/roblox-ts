aeb2401e9efc21fbefc36c9e555d4e8a1263d9ea
osyrisrblx <osyrisrblx@gmail.com>
2020-11-03 17:02:32 -0800

    Remove unused file

--- src/LuauAST/bundle.ts
+++ src/LuauAST/bundle.ts
@@ -1,7 +1,6 @@
 export * from "LuauAST/impl/create";
 export * from "LuauAST/impl/enums";
 export * from "LuauAST/impl/List";
-export * from "LuauAST/impl/traversal";
 export * from "LuauAST/impl/typeGuards";
 export * from "LuauAST/types/mapping";
 export * from "LuauAST/types/nodes";
