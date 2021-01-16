a6296e144b9ba1bece8ba3f3a90642ca13bce6aa
Dion <39664950+Dionysusnu@users.noreply.github.com>
2021-01-16 03:30:13 +0100

    Fix `array | undefined` index not being incremented (#1226)

--- src/TSTransformer/util/addOneIfArrayType.ts
+++ src/TSTransformer/util/addOneIfArrayType.ts
@@ -2,14 +2,14 @@ import ts from "byots";
 import luau from "LuauAST";
 import { TransformState } from "TSTransformer";
 import { offset } from "TSTransformer/util/offset";
-import { isArrayType, isDefinitelyType } from "TSTransformer/util/types";
+import { isArrayType, isDefinitelyType, isUndefinedType } from "TSTransformer/util/types";
 
 export function addOneIfArrayType(
 	state: TransformState,
 	type: ts.Type | ReadonlyArray<ts.Type>,
 	expression: luau.Expression,
 ) {
-	if (ts.isArray(type) || isDefinitelyType(type, t => isArrayType(state, t))) {
+	if (ts.isArray(type) || isDefinitelyType(type, t => isArrayType(state, t) || isUndefinedType(t))) {
 		return offset(expression, 1);
 	} else {
 		return expression;
