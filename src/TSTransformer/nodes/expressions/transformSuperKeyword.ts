db2fd1d2a907b17dee1ea04c96ef9eba66607386
osyrisrblx <osyrisrblx@gmail.com>
2021-02-14 23:36:11 -0800

    use luau.globals.super instead of magic "super" id

--- src/TSTransformer/nodes/expressions/transformSuperKeyword.ts
+++ src/TSTransformer/nodes/expressions/transformSuperKeyword.ts
@@ -3,5 +3,5 @@ import luau from "LuauAST";
 import { TransformState } from "TSTransformer";
 
 export function transformSuperKeyword(state: TransformState, node: ts.Token<ts.SyntaxKind.SuperKeyword>) {
-	return luau.id("super");
+	return luau.globals.super;
 }
