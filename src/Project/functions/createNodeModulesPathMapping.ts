7a24901797dd8a29ffa927bfe2c2498bda70296b
osyrisrblx <osyrisrblx@gmail.com>
2021-03-06 13:54:39 -0800

    Make RoactSymbolManager more generic

--- src/Project/functions/createNodeModulesPathMapping.ts
+++ src/Project/functions/createNodeModulesPathMapping.ts
@@ -1,5 +1,6 @@
 import fs from "fs-extra";
 import path from "path";
+import { realPathExistsSync } from "Shared/util/realPathExistsSync";
 
 export function createNodeModulesPathMapping(nodeModulesPath: string) {
 	const nodeModulesPathMapping = new Map<string, string>();
@@ -7,23 +8,17 @@ export function createNodeModulesPathMapping(nodeModulesPath: string) {
 		// map module paths
 		for (const pkgName of fs.readdirSync(nodeModulesPath)) {
 			const pkgPath = path.join(nodeModulesPath, pkgName);
-			const pkgJsonPath = path.join(pkgPath, "package.json");
-			if (fs.existsSync(pkgJsonPath)) {
-				const pkgJsonRealPath = fs.realpathSync(pkgJsonPath);
-				if (fs.existsSync(pkgJsonRealPath)) {
-					const pkgJson = fs.readJsonSync(pkgJsonRealPath) as {
-						main?: string;
-						typings?: string;
-						types?: string;
-					};
-					// both "types" and "typings" are valid
-					const typesPath = pkgJson.types ?? pkgJson.typings ?? "index.d.ts";
-					if (pkgJson.main) {
-						nodeModulesPathMapping.set(
-							path.resolve(pkgPath, typesPath),
-							path.resolve(pkgPath, pkgJson.main),
-						);
-					}
+			const pkgJsonPath = realPathExistsSync(path.join(pkgPath, "package.json"));
+			if (pkgJsonPath !== undefined) {
+				const pkgJson = fs.readJsonSync(pkgJsonPath) as {
+					main?: string;
+					typings?: string;
+					types?: string;
+				};
+				// both "types" and "typings" are valid
+				const typesPath = pkgJson.types ?? pkgJson.typings ?? "index.d.ts";
+				if (pkgJson.main) {
+					nodeModulesPathMapping.set(path.resolve(pkgPath, typesPath), path.resolve(pkgPath, pkgJson.main));
 				}
 			}
 		}
