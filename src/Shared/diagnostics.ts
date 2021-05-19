79b8e47d458f5afb4b8e436830db2faa98461a12
Dion <39664950+Dionysusnu@users.noreply.github.com>
2021-05-19 19:09:53 +0200

    Fix assertion failure with complex for-of variable list (#1392)
    Fix assertion failure with complex for-of variable list

--- src/Shared/diagnostics.ts
+++ src/Shared/diagnostics.ts
@@ -140,6 +140,7 @@ export const errors = {
 	noUnaryPlus: error("Unary `+` is not supported!", suggestion("Use `tonumber(x)` instead.")),
 	noNonNumberUnaryMinus: error("Unary `-` is only supported for number types!"),
 	noAwaitForOf: error("`await` is not supported in for-of loops!"),
+	noComplexForOf: error("for-of loops do not support complex variable lists!", issue(1253)),
 	noAsyncGeneratorFunctions: error("Async generator functions are not supported!"),
 	noNonStringModuleSpecifier: error("Module specifiers must be a string literal."),
 	noIterableIteration: error("Iterating on Iterable<T> is not supported! You must use a more specific type."),
