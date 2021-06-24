cf155547b0dfe8e3c6b8dea625ca55aa29aedd56
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-06-24 22:41:20 +0200

    Add assert check

--- src/LuauRenderer/nodes/fields/renderMapField.ts
+++ src/LuauRenderer/nodes/fields/renderMapField.ts
@@ -1,5 +1,6 @@
 import luau from "LuauAST";
 import { render, RenderState } from "LuauRenderer";
+import { assert } from "Shared/util/assert";
 
 export function renderMapField(state: RenderState, node: luau.MapField) {
 	const { index, value } = node;
@@ -7,6 +8,7 @@ export function renderMapField(state: RenderState, node: luau.MapField) {
 	if (luau.isStringLiteral(index) && luau.isValidIdentifier(index.value)) {
 		return `${index.value} = ${valueStr}`;
 	} else {
+		assert(!luau.isComplexStringLiteral(index), "Complex strings not allowed as table literal keys");
 		const indexStr = render(state, index);
 		return `[${indexStr}] = ${valueStr}`;
 	}
