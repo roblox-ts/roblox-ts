9c8f7ce002d89fd0b3541c961157917c8d4582d8
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-06-24 23:14:21 +0200

    Add check in renderComputedIndexExpression

--- src/LuauRenderer/nodes/expressions/indexable/renderComputedIndexExpression.ts
+++ src/LuauRenderer/nodes/expressions/indexable/renderComputedIndexExpression.ts
@@ -1,11 +1,13 @@
 import luau from "LuauAST";
 import { render, RenderState } from "LuauRenderer";
+import { assert } from "Shared/util/assert";
 
 export function renderComputedIndexExpression(state: RenderState, node: luau.ComputedIndexExpression) {
 	const expStr = render(state, node.expression);
 	if (luau.isStringLiteral(node.index) && luau.isValidIdentifier(node.index.value)) {
 		return `${expStr}.${node.index.value}`;
 	} else {
+		assert(!luau.isComplexStringLiteral(node.index), "Complex string not allowed as index");
 		const indexStr = render(state, node.index);
 		return `${expStr}[${indexStr}]`;
 	}
