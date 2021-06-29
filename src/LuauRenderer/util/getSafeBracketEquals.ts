91d71c08d2ac62c9fbc409e3e1ef829f4f7bc1da
osyrisrblx <osyrisrblx@gmail.com>
2021-06-29 15:00:40 -0700

    Remove accidental whitespace

--- src/LuauRenderer/util/getSafeBracketEquals.ts
+++ src/LuauRenderer/util/getSafeBracketEquals.ts
@@ -1,6 +1,5 @@
 export function getSafeBracketEquals(str: string) {
 	let amtEquals = 0;
-
 	while (str.includes(`]${"=".repeat(amtEquals)}]`) || str.endsWith(`]${"=".repeat(amtEquals)}`)) {
 		amtEquals++;
 	}
