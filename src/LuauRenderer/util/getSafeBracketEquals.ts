9f72f97bac77ca510c2b651ae8eed88ee8199265
osyrisrblx <osyrisrblx@gmail.com>
2021-06-29 14:55:32 -0700

    Fix #1467

--- src/LuauRenderer/util/getSafeBracketEquals.ts
+++ src/LuauRenderer/util/getSafeBracketEquals.ts
@@ -1,5 +1,10 @@
 export function getSafeBracketEquals(str: string) {
 	let amtEquals = 0;
+
+	if (str.endsWith("]")) {
+		amtEquals++;
+	}
+
 	while (str.includes(`]${"=".repeat(amtEquals)}]`)) {
 		amtEquals++;
 	}
