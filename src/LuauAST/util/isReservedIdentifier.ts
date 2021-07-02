568f30e9edef5bb59602a9377148538e0779bec6
osyrisrblx <osyrisrblx@gmail.com>
2021-07-01 19:04:34 -0700

    Simplify isReservedIdentifier logic

--- src/LuauAST/util/isReservedIdentifier.ts
+++ src/LuauAST/util/isReservedIdentifier.ts
@@ -1,11 +1,5 @@
 import { globals } from "LuauAST/impl/globals";
 
-const RESERVED_GLOBALS = new Set<string>();
-
-for (const globalName of Object.keys(globals)) {
-	RESERVED_GLOBALS.add(globalName);
-}
-
 export function isReservedIdentifier(id: string) {
-	return RESERVED_GLOBALS.has(id);
+	return id in globals;
 }
