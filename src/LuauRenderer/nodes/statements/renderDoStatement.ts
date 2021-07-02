bac4a5fbf45917421e0936d74c12ec258930fba2
osyrisrblx <osyrisrblx@gmail.com>
2021-07-01 17:42:37 -0700

    Fix quote consistency

--- src/LuauRenderer/nodes/statements/renderDoStatement.ts
+++ src/LuauRenderer/nodes/statements/renderDoStatement.ts
@@ -4,8 +4,8 @@ import { renderStatements } from "LuauRenderer/util/renderStatements";
 
 export function renderDoStatement(state: RenderState, node: luau.DoStatement) {
 	let result = "";
-	result += state.line("do");
+	result += state.line(`do`);
 	result += state.scope(() => renderStatements(state, node.statements));
-	result += state.line("end");
+	result += state.line(`end`);
 	return result;
 }
