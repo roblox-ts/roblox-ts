7bd20ac6744a68088aee25e1c75cb61b574c06b3
osyrisrblx <osyrisrblx@gmail.com>
2021-07-07 21:50:25 -0700

    Fix copyInclude for packages

--- src/Project/functions/copyInclude.ts
+++ src/Project/functions/copyInclude.ts
@@ -1,10 +1,14 @@
 import fs from "fs-extra";
-import { LIB_PATH } from "Shared/constants";
+import { LIB_PATH, ProjectType } from "Shared/constants";
 import { ProjectData } from "Shared/types";
 import { benchmarkIfVerbose } from "Shared/util/benchmark";
 
 export function copyInclude(data: ProjectData) {
-	if (!data.noInclude) {
+	if (
+		!data.noInclude &&
+		data.projectOptions.type !== ProjectType.Package &&
+		!(data.projectOptions.type === undefined && data.isPackage)
+	) {
 		benchmarkIfVerbose("copy include files", () => fs.copySync(LIB_PATH, data.includePath, { dereference: true }));
 	}
 }
