18ec6445c7ff61f47e5545d628d65f327621b0f0
osyrisrblx <osyrisrblx@gmail.com>
2021-07-13 13:25:36 -0700

    Fix assertion failure

--- src/TSTransformer/util/isDefinedAsLet.ts
+++ src/TSTransformer/util/isDefinedAsLet.ts
@@ -1,14 +1,16 @@
 import ts from "byots";
-import { assert } from "Shared/util/assert";
 import { getOrSetDefault } from "Shared/util/getOrSetDefault";
 import { TransformState } from "TSTransformer";
 import { getAncestor } from "TSTransformer/util/traversal";
 
 export function isDefinedAsLet(state: TransformState, idSymbol: ts.Symbol) {
 	return getOrSetDefault(state.multiTransformState.isDefinedAsLetCache, idSymbol, () => {
-		assert(idSymbol.valueDeclaration);
-		return !!(
-			(getAncestor(idSymbol.valueDeclaration, ts.isVariableDeclarationList)?.flags ?? 0) & ts.NodeFlags.Let
-		);
+		if (idSymbol.valueDeclaration) {
+			const varDecList = getAncestor(idSymbol.valueDeclaration, ts.isVariableDeclarationList);
+			if (varDecList) {
+				return !!(varDecList.flags & ts.NodeFlags.Let);
+			}
+		}
+		return false;
 	});
 }
