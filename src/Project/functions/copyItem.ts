6bba3cbae3d240a8c74c6be8e4fcac5169f3b6cf
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-08-12 19:10:32 +0200

    Fix declaration file checking

--- src/Project/functions/copyItem.ts
+++ src/Project/functions/copyItem.ts
@@ -1,6 +1,6 @@
 import fs from "fs-extra";
 import { ProjectData } from "Project";
-import { checkFilename } from "Project/functions/checkFileName";
+import { checkDeclarationFileFilename } from "Project/functions/checkFileName";
 import { isCompilableFile } from "Project/util/isCompilableFile";
 import { PathTranslator } from "Shared/classes/PathTranslator";
 import { DTS_EXT } from "Shared/constants";
@@ -8,7 +8,10 @@ import { DTS_EXT } from "Shared/constants";
 export function copyItem(data: ProjectData, pathTranslator: PathTranslator, item: string) {
 	fs.copySync(item, pathTranslator.getOutputPath(item), {
 		filter: (src, dest) => {
-			checkFilename(src);
+			if (src.endsWith(DTS_EXT)) {
+				checkDeclarationFileFilename(src);
+			}
+
 			if (
 				data.writeOnlyChanged &&
 				fs.pathExistsSync(dest) &&
