16d9ba0c97b1a467e5fb51a73c294bbb291af131
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-08-12 19:10:20 +0200

    use pre emit diagnostic

--- src/Project/functions/checkFileName.ts
+++ src/Project/functions/checkFileName.ts
@@ -1,12 +1,28 @@
+import ts from "byots";
 import path from "path";
 import { FILENAME_WARNINGS } from "Shared/constants";
 import { warnings } from "Shared/diagnostics";
 import { DiagnosticService } from "TSTransformer/classes/DiagnosticService";
 
-export function checkFilename(filePath: string) {
+function checkFilename(filePath: string) {
 	const baseName = path.basename(filePath);
 	const nameWarning = FILENAME_WARNINGS.get(baseName);
 	if (nameWarning) {
-		DiagnosticService.addDiagnostic(warnings.incorrectRootFilename(baseName, nameWarning, filePath));
+		return warnings.incorrectRootFilename(baseName, nameWarning, filePath);
+	}
+}
+
+export function checkSourceFileFilename(sourceFile: ts.SourceFile) {
+	const nameWarning = checkFilename(sourceFile.fileName);
+	if (nameWarning) {
+		return [nameWarning];
+	}
+	return [];
+}
+
+export function checkDeclarationFileFilename(fileName: string) {
+	const nameWarning = checkFilename(fileName);
+	if (nameWarning) {
+		DiagnosticService.addDiagnostic(nameWarning);
 	}
 }
--- src/Project/util/getCustomPreEmitDiagnostics.ts
+++ src/Project/util/getCustomPreEmitDiagnostics.ts
@@ -1,8 +1,9 @@
 import ts from "byots";
+import { checkSourceFileFilename } from "Project/functions/checkFileName";
 import { fileIsModule } from "TSTransformer/preEmitDiagnostics/fileIsModule";
 
 export type PreEmitChecker = (sourceFile: ts.SourceFile) => Array<ts.Diagnostic>;
-const PRE_EMIT_DIAGNOSTICS: Array<PreEmitChecker> = [fileIsModule];
+const PRE_EMIT_DIAGNOSTICS: Array<PreEmitChecker> = [fileIsModule, checkSourceFileFilename];
 
 export function getCustomPreEmitDiagnostics(sourceFile: ts.SourceFile) {
 	const diagnostics = new Array<ts.Diagnostic>();
