2adab462942fda35663d6753e292b9c6e1839e3c
Osyris <osyrisrblx@gmail.com>
2021-08-12 00:48:15 -0700

    Fix #1527 (#1564)

--- src/TSTransformer/typeGuards.ts
+++ src/TSTransformer/typeGuards.ts
@@ -15,3 +15,7 @@ export function isUnaryAssignmentOperator(
 ): operator is ts.SyntaxKind.PlusPlusToken | ts.SyntaxKind.MinusMinusToken {
 	return operator === ts.SyntaxKind.PlusPlusToken || operator === ts.SyntaxKind.MinusMinusToken;
 }
+
+export function isTemplateLiteralType(type: ts.Type): type is ts.TemplateLiteralType {
+	return "texts" in type && "types" in type && !!(type.flags & ts.TypeFlags.TemplateLiteral);
+}
