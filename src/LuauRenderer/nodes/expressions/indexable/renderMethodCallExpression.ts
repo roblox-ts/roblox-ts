277d8a87fac58da97ce6c02d23b6232dc82dba8f
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-09-16 19:56:12 +0200

    assert method call name is valid identifier

--- src/LuauRenderer/nodes/expressions/indexable/renderMethodCallExpression.ts
+++ src/LuauRenderer/nodes/expressions/indexable/renderMethodCallExpression.ts
@@ -1,7 +1,9 @@
 import luau from "LuauAST";
 import { render, RenderState } from "LuauRenderer";
 import { renderArguments } from "LuauRenderer/util/renderArguments";
+import { assert } from "Shared/util/assert";
 
 export function renderMethodCallExpression(state: RenderState, node: luau.MethodCallExpression) {
+	assert(luau.isValidIdentifier(node.name));
 	return `${render(state, node.expression)}:${node.name}(${renderArguments(state, node.args)})`;
 }
