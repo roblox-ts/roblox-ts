c91b9d2d8f346e2d06570df254538ce29ceddee5
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-10-05 22:24:32 +0200

    Add diagnostic for property macro destructuring

--- src/TSTransformer/util/binding/objectAccessor.ts
+++ src/TSTransformer/util/binding/objectAccessor.ts
@@ -1,16 +1,30 @@
 import luau from "LuauAST";
+import { errors } from "Shared/diagnostics";
 import { assert } from "Shared/util/assert";
 import { TransformState } from "TSTransformer";
+import { DiagnosticService } from "TSTransformer/classes/DiagnosticService";
 import { transformExpression } from "TSTransformer/nodes/expressions/transformExpression";
 import { addOneIfArrayType } from "TSTransformer/util/addOneIfArrayType";
+import { getKindName } from "TSTransformer/util/getKindName";
+import { getFirstDefinedSymbol } from "TSTransformer/util/types";
 import ts from "typescript";
 
 export const objectAccessor = (
 	state: TransformState,
 	parentId: luau.AnyIdentifier,
 	accessType: ts.Type | ReadonlyArray<ts.Type>,
-	name: ts.Node,
+	name:
+		| ts.Identifier
+		| ts.ComputedPropertyName
+		| ts.NumericLiteral
+		| ts.StringLiteral
+		| ts.PrivateIdentifier
+		| ts.BindingPattern,
 ): luau.Expression => {
+	const symbol = getFirstDefinedSymbol(state, state.getType(name));
+	if (symbol && state.services.macroManager.getPropertyCallMacro(symbol)) {
+		DiagnosticService.addDiagnostic(errors.noIndexWithoutCall(name.parent));
+	}
 	if (ts.isIdentifier(name)) {
 		return luau.property(parentId, name.text);
 	} else if (ts.isComputedPropertyName(name)) {
@@ -23,6 +37,7 @@ export const objectAccessor = (
 			expression: parentId,
 			index: transformExpression(state, name),
 		});
+	} else {
+		assert(false, `Uncaught node kind ${getKindName(name.kind)} in objectAccessor`);
 	}
-	assert(false);
 };
--- tests/src/diagnostics/noIndexWithoutCall.3.ts
+++ tests/src/diagnostics/noIndexWithoutCall.3.ts
@@ -2,3 +2,6 @@ export {};
 
 const array = [1, 2, 3];
 const { push } = array;
+const otherArray = [1, 2];
+otherArray.push = push;
+otherArray.push();
