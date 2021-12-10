7b5cd0d7db4ea6efe6da487b00cfe11811a3577e
Dion <39664950+Dionysusnu@users.noreply.github.com>
2021-12-10 07:52:48 +0100

    Allow method index without call for typeIs macro (#1696)
    * Allow method index without call for typeIs macro
    
    * Fix logic bug returning false early

--- src/TSTransformer/util/isValidMethodIndexWithoutCall.ts
+++ src/TSTransformer/util/isValidMethodIndexWithoutCall.ts
@@ -1,6 +1,10 @@
+import { TransformState } from "TSTransformer";
+import { CALL_MACROS } from "TSTransformer/macros/callMacros";
+import { getFirstDefinedSymbol } from "TSTransformer/util/types";
 import ts from "typescript";
 
-export function isValidMethodIndexWithoutCall(parent: ts.Node): boolean {
+export function isValidMethodIndexWithoutCall(state: TransformState, node: ts.Node): boolean {
+	const { parent } = node;
 	// a.b !== undefined
 	if (ts.isBinaryExpression(parent)) {
 		return true;
@@ -9,6 +13,22 @@ export function isValidMethodIndexWithoutCall(parent: ts.Node): boolean {
 	// !a.b
 	if (ts.isPrefixUnaryExpression(parent)) {
 		return true;
+	}
+
+	// typeIs/typeOf macros
+	if (ts.isCallExpression(parent)) {
+		const expType = state.typeChecker.getNonOptionalType(state.getType(parent.expression));
+		const symbol = getFirstDefinedSymbol(state, expType);
+		if (symbol) {
+			const macro = state.services.macroManager.getCallMacro(symbol);
+			if (
+				// typeIs will be a TypeError if usage is not the first argument
+				macro === CALL_MACROS.typeIs ||
+				macro === CALL_MACROS.typeOf
+			) {
+				return true;
+			}
+		}
 	}
 
 	return false;
