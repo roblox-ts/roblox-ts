d489418ba19d6a595cdd8ba6fa07dd90c36221b0
Dion <39664950+Dionysusnu@users.noreply.github.com>
2021-12-10 07:52:43 +0100

    Re-add JSX compilerOptions checks (#1690)

--- src/TSTransformer/classes/MultiTransformState.ts
+++ src/TSTransformer/classes/MultiTransformState.ts
@@ -1,3 +1,5 @@
+import { errors } from "Shared/diagnostics";
+import { DiagnosticService } from "TSTransformer/classes/DiagnosticService";
 import ts from "typescript";
 
 /**
@@ -9,6 +11,21 @@ export class MultiTransformState {
 	public readonly isReportedByNoAnyCache = new Set<ts.Symbol>();
 	public readonly getModuleExportsCache = new Map<ts.Symbol, Array<ts.Symbol>>();
 	public readonly getModuleExportsAliasMapCache = new Map<ts.Symbol, Map<ts.Symbol, string>>();
+
+	public hasCheckedJsxFactory = false;
+	public checkJsxFactory(node: ts.Node, compilerOptions: ts.CompilerOptions) {
+		this.hasCheckedJsxFactory = true;
+		if (!this.hasCheckedJsxFactory && compilerOptions.jsxFactory !== "Roact.createElement") {
+			DiagnosticService.addDiagnostic(errors.invalidJsxFactory(node));
+		}
+	}
+	public hasCheckedJsxFragmentFactory = false;
+	public checkJsxFragmentFactory(node: ts.Node, compilerOptions: ts.CompilerOptions) {
+		this.hasCheckedJsxFragmentFactory = true;
+		if (!this.hasCheckedJsxFragmentFactory && compilerOptions.jsxFragmentFactory !== "Roact.createFragment") {
+			DiagnosticService.addDiagnostic(errors.invalidJsxFragmentFactory(node));
+		}
+	}
 
 	constructor() {}
 }
--- src/TSTransformer/nodes/expressions/transformJsxFragment.ts
+++ src/TSTransformer/nodes/expressions/transformJsxFragment.ts
@@ -1,7 +1,20 @@
+import luau from "LuauAST";
 import { TransformState } from "TSTransformer";
-import { transformJsxFragmentShorthand } from "TSTransformer/nodes/jsx/transformJsx";
+import { transformJsxChildren } from "TSTransformer/nodes/jsx/transformJsxChildren";
+import { createRoactIndex } from "TSTransformer/util/jsx/createRoactIndex";
+import { createMapPointer, createMixedTablePointer } from "TSTransformer/util/pointer";
 import ts from "typescript";
 
 export function transformJsxFragment(state: TransformState, node: ts.JsxFragment) {
-	return transformJsxFragmentShorthand(state, node.children);
+	state.checkJsxFragmentFactory(node);
+
+	const childrenPtr = createMixedTablePointer("children");
+	transformJsxChildren(state, node.children, createMapPointer("attributes"), childrenPtr);
+
+	const args = new Array<luau.Expression>();
+	if (luau.isAnyIdentifier(childrenPtr.value) || !luau.list.isEmpty(childrenPtr.value.fields)) {
+		args.push(childrenPtr.value);
+	}
+
+	return luau.call(createRoactIndex("createFragment"), args);
 }
