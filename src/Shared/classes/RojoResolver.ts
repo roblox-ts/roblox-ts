3631ffc549f2a0efc1db139445c0d85a571affad
osyrisrblx <osyrisrblx@gmail.com>
2022-01-01 17:13:04 -0800

    Improve RojoTreeMembers definition

--- src/Shared/classes/RojoResolver.ts
+++ src/Shared/classes/RojoResolver.ts
@@ -30,7 +30,7 @@ interface RojoTreeMetadata {
 type RojoTree = RojoTreeMetadata & RojoTreeMembers;
 
 interface RojoTreeMembers {
-	[name: string]: RojoTree;
+	[property: `$${string}`]: RojoTree;
 }
 
 interface RojoFile {
@@ -194,7 +194,7 @@ export class RojoResolver {
 		}
 
 		for (const childName of Object.keys(tree).filter(v => !v.startsWith("$"))) {
-			this.parseTree(basePath, childName, tree[childName]);
+			this.parseTree(basePath, childName, tree[childName as `$${string}`]);
 		}
 
 		if (!doNotPush) this.rbxPath.pop();
