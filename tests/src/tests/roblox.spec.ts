e4295418cb1da02cc720362cba2b97700a7b721a
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2022-01-06 01:55:31 -0500

    Fix element call expression on Roblox types (#1761)
    * Fix element call expression on Roblox types
    
    * Improve whitespace
    
    * Add unit tests
    
    Co-authored-by: osyrisrblx <osyrisrblx@gmail.com>

---
+++ tests/src/tests/roblox.spec.ts
@@ -0,0 +1,11 @@
+/// <reference types="@rbxts/testez/globals" />
+
+export = () => {
+	it("should support using the Roblox API", () => {
+		expect(game.FindFirstChild("Workspace")).to.equal(game.GetService("Workspace"));
+	});
+
+	it("should support calling Roblox API methods with element expressions", () => {
+		expect(game["FindFirstChild"]("Workspace")).to.equal(game["GetService"]("Workspace"));
+	});
+};
