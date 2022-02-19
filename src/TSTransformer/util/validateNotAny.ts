2309763c3815c1ce2cb48dd1c292d88b771845d5
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-02-19 15:04:52 +0100

    Add clarification comment

--- src/TSTransformer/util/validateNotAny.ts
+++ src/TSTransformer/util/validateNotAny.ts
@@ -3,7 +3,7 @@ import { isDefinitelyType } from "TSTransformer/util/types";
 import ts from "typescript";
 
 /**
- * Should only be used in cases where any counts for TS correctness.
+ * Should only be used in cases where `any` counts for TS correctness.
  * For example, `a++` is valid if `a` is `number` or `any`, but nothing else.
  * In this case, we only need to check that the type is not `any`
  * to guarantee that it will be a `number`.
@@ -12,6 +12,10 @@ import ts from "typescript";
 export function validateNotAnyType(state: TransformState, node: ts.Node) {
 	// noAny checks are automatically done in isDefinitelyType
 	// therefore, calling it will report the diagnostic
-	// and we can discard the result
+	// and we can discard the result.
+
+	// We don't need to pass any typechecks
+	// Since `any` is never a union/intersection type
+	// And the case for single types checks for `any` directly
 	isDefinitelyType(state, state.getType(node), node);
 }
