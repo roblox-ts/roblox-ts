c61c289c73ee746ede8746046589e5533762b52b
Dion <39664950+Dionysusnu@users.noreply.github.com>
2022-04-08 23:14:18 +0200

    Update getCustomPreEmitDiagnostics.ts

--- src/Project/util/getCustomPreEmitDiagnostics.ts
+++ src/Project/util/getCustomPreEmitDiagnostics.ts
@@ -2,7 +2,7 @@ import { fileIsModule } from "TSTransformer/preEmitDiagnostics/fileIsModule";
 import ts from "typescript";
 
 export type PreEmitChecker = (sourceFile: ts.SourceFile) => Array<ts.Diagnostic>;
-const PRE_EMIT_DIAGNOSTICS: Array<PreEmitChecker> = [fileIsModule];
+const PRE_EMIT_DIAGNOSTICS: Array<PreEmitChecker> = [];
 
 export function getCustomPreEmitDiagnostics(sourceFile: ts.SourceFile) {
 	const diagnostics = new Array<ts.Diagnostic>();
