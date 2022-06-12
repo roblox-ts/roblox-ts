64756d439f56d7ac92c239de8a34d9219f5e574c
Osyris <osyrisrblx@gmail.com>
2022-06-12 00:40:12 -0400

    `$range` macro (#1979)
    * Fix #1807
    
    * Add diagnostic for use outside for-of
    
    * Simplify implementation
    
    * Remove unnecessary extra variable

--- src/Shared/diagnostics.ts
+++ src/Shared/diagnostics.ts
@@ -173,6 +173,7 @@ export const errors = {
 		suggestion("Did you mean to use an array spread? `[ ...exp ]`"),
 	),
 	noVarArgsMacroSpread: error("Macros which use variadric arguments do not support spread expressions!", issue(1149)),
+	noRangeMacroOutsideForOf: error("$range() macro is only valid as an expression of a for-of loop!"),
 
 	// import/export
 	noModuleSpecifierFile: error("Could not find file for import. Did you forget to `npm install`?"),
