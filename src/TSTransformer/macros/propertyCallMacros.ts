e6c1bd2e5965a724188829e909c0a38540cc3082
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2022-06-14 00:20:11 +0200

    Fix disappearing array.push() call regression

--- src/TSTransformer/macros/propertyCallMacros.ts
+++ src/TSTransformer/macros/propertyCallMacros.ts
@@ -600,7 +600,8 @@ const ARRAY_METHODS: MacroList<PropertyCallMacro> = {
 			);
 		}
 
-		return !isUsedAsStatement(node) ? luau.unary("#", expression) : luau.none();
+		// for `a.push()` always emit luau.unary so the call doesn't disappear in emit
+		return !isUsedAsStatement(node) || args.length === 0 ? luau.unary("#", expression) : luau.none();
 	},
 
 	pop: (state, node, expression) => {
