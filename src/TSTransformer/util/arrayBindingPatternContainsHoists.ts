736c34c5bf33f493a11ec2e00f5e191bbb3ec2bd
osyrisrblx <osyrisrblx@gmail.com>
2022-06-12 20:06:12 -0400

    Add comment

--- src/TSTransformer/util/arrayBindingPatternContainsHoists.ts
+++ src/TSTransformer/util/arrayBindingPatternContainsHoists.ts
@@ -7,6 +7,9 @@ export function arrayBindingPatternContainsHoists(
 	arrayBindingPattern: ts.ArrayBindingPattern,
 ): boolean {
 	for (const element of arrayBindingPattern.elements) {
+		// If it's not a ts.BindingElement, it must be a ts.OmittedExpression. In that case, doesn't create a variable.
+		// For non-identifiers, element.name must be a nested array/object binding pattern.
+		// For those cases, the hoisting logic is handled elsewhere and the variable here will be a tempId.
 		if (ts.isBindingElement(element) && ts.isIdentifier(element.name)) {
 			const symbol = state.typeChecker.getSymbolAtLocation(element.name);
 			if (symbol) {
