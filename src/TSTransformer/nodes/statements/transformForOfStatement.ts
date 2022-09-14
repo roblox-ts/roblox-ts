745e33cec687315b4d853d722bbb5a8104547587
Dion <39664950+Dionysusnu@users.noreply.github.com>
2022-09-14 18:49:05 +0200

    Rename variable

--- src/TSTransformer/nodes/statements/transformForOfStatement.ts
+++ src/TSTransformer/nodes/statements/transformForOfStatement.ts
@@ -459,7 +459,7 @@ export function transformForOfRangeMacro(
 	const statements = luau.list.make<luau.Statement>();
 	const id = transformForInitializer(state, node.initializer, statements);
 
-	const [[start, end, step], prereqs] = state.capture(() => ensureTransformOrder(state, node.expression.arguments));
+	const [[start, end, stepExp], prereqs] = state.capture(() => ensureTransformOrder(state, node.expression.arguments));
 	luau.list.pushList(result, prereqs);
 
 	luau.list.pushList(statements, transformStatementList(state, getStatements(node.statement)));
@@ -470,8 +470,8 @@ export function transformForOfRangeMacro(
 			id,
 			start,
 			end,
-			step: step === undefined || luau.isNumberLiteral(step)
-				? step
+			step: stepExp === undefined || luau.isNumberLiteral(step)
+				? stepExp
 				: luau.binary(step, "or", luau.number(1)),
 			statements,
 		}),
