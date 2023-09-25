0a4a566fbe356f6a23ab492d711e2e4ed608e44d
Albert Hong <107275993+alberrttt@users.noreply.github.com>
2023-09-24 19:31:58 -0700

    fix eslint warning

--- src/TSTransformer/nodes/transformOptionalChain.ts
+++ src/TSTransformer/nodes/transformOptionalChain.ts
@@ -260,7 +260,7 @@ function transformOptionalChainInner(
 				if (item.kind === OptionalChainItemKind.PropertyCall) {
 					baseExpression = luau.property(convertToIndexableExpression(baseExpression), item.name);
 				} else if (item.kind === OptionalChainItemKind.ElementCall) {
-					const [index, prereqs] = state.capture(() => transformExpression(state, item.argumentExpression));
+					const [index] = state.capture(() => transformExpression(state, item.argumentExpression));
 					const expType = state.typeChecker.getNonOptionalType(state.getType(item.expression.expression));
 
 					baseExpression = luau.create(luau.SyntaxKind.ComputedIndexExpression, {
