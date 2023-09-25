2f176c57236b292e62a44de4891c819ef7d0828e
Albert Hong <107275993+alberrttt@users.noreply.github.com>
2023-09-25 16:57:22 -0700

    static block declarations

--- src/TSTransformer/nodes/expressions/transformThisExpression.ts
+++ src/TSTransformer/nodes/expressions/transformThisExpression.ts
@@ -2,6 +2,7 @@ import luau from "@roblox-ts/luau-ast";
 import { errors } from "Shared/diagnostics";
 import { SYMBOL_NAMES, TransformState } from "TSTransformer";
 import { DiagnosticService } from "TSTransformer/classes/DiagnosticService";
+import assert from "assert";
 import ts from "typescript";
 
 export function transformThisExpression(state: TransformState, node: ts.ThisExpression) {
@@ -10,5 +11,18 @@ export function transformThisExpression(state: TransformState, node: ts.ThisExpr
 		DiagnosticService.addDiagnostic(errors.noGlobalThis(node));
 	}
 
+	if (state.isInStaticBlockDeclaration) {
+		assert(symbol);
+		const type = state.typeChecker.getTypeOfSymbolAtLocation(symbol, node);
+		const symbolDeclaration = symbol.getDeclarations()?.[0];
+		assert(symbolDeclaration);
+		assert(ts.isClassDeclaration(symbolDeclaration));
+
+		const className = symbolDeclaration.name;
+		assert(className);
+
+		return luau.id(className.text);
+	} else {
+	}
 	return luau.globals.self;
 }
