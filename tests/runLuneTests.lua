318425efd03b9ee8623ea05661b110dcd7d7ddac
Osyris <osyrisrblx@gmail.com>
2023-10-15 23:10:35 -0400

    Lune Unit Testing (#2489)
    Switches to [Lune](https://github.com/filiptibell/lune) for unit
    testing. This should be more stable than the old run-in-roblox flow, but
    requires us to implement Roblox API shims.
    
    ## Remaining Issues
    (need to be resolved before merging this PR + reverting some hacks)
    
    - [x] `:WaitForChild()` missing
    - [x] `RunService.Heartbeat` missing and errors on index inside
    Promise.lua
    - [x] `TestService:Error()` missing, used by TestEZ

---
+++ tests/runLuneTests.lua
@@ -0,0 +1,76 @@
+local roblox = require("@lune/roblox")
+local fs = require("@lune/fs")
+local luau = require("@lune/luau")
+local process = require("@lune/process")
+local stdio = require("@lune/stdio")
+
+local testPlacePath = process.args[1]
+
+local game = roblox.deserializePlace(fs.readFile(testPlacePath))
+
+local function tableJoin(...)
+	local result = {}
+	for i = 1, select("#", ...) do
+		for k, v in select(i, ...) do
+			result[k] = v
+		end
+	end
+	return result
+end
+
+-- not 100% accurate to tick() functionality, but good enough for TestEZ usage
+local function tick()
+	return os.clock()
+end
+
+-- roblox.spec.ts assumes Workspace already exists
+game:GetService("Workspace")
+
+-- RuntimeLib uses :WaitForChild(), but tests don't need networking so :FindFirstChild() should be fine
+roblox.implementMethod("Instance", "WaitForChild", function(self, ...)
+	return self:FindFirstChild(...)
+end)
+
+-- TestEZ uses TestService:Error() when tests fail
+roblox.implementMethod("TestService", "Error", function(description: string, source: Instance?, line: number?)
+	stdio.ewrite(`{description}\n`)
+end)
+
+-- Promise.lua indexes RunService.Heartbeat, but only uses it in Promise.defer and Promise.delay
+roblox.implementProperty("RunService", "Heartbeat", function()
+	return {}
+end, function() end)
+
+local robloxRequire
+
+local function runRobloxScript(script: LuaSourceContainer)
+	local callableFn = luau.load(luau.compile(script.Source), {
+		debugName = script:GetFullName(),
+		environment = tableJoin(roblox, {
+			game = game,
+			script = script,
+			require = robloxRequire,
+			tick = tick,
+		}),
+	})
+
+	return callableFn()
+end
+
+local requireCache = {}
+
+function robloxRequire(moduleScript: ModuleScript)
+	-- the same script instance sometimes gives a different ref
+	-- unsure why, but using :GetFullName() fixes this for now
+	local scriptPath = moduleScript:GetFullName()
+	local cached = requireCache[scriptPath]
+	if cached then
+		return table.unpack(cached)
+	end
+
+	local result = table.pack(runRobloxScript(moduleScript))
+	requireCache[scriptPath] = result
+	return table.unpack(result)
+end
+
+runRobloxScript(game.ServerScriptService.main)
