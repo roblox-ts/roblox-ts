c3504163c9ea6251b24342fb6d52a756677e3688
osyrisrblx <osyrisrblx@gmail.com>
2024-02-10 15:14:45 -0500

    Add overloads to prevent unnecessary calls to convertToIndexableExpression

--- src/TSTransformer/util/convertToIndexableExpression.ts
+++ src/TSTransformer/util/convertToIndexableExpression.ts
@@ -1,6 +1,10 @@
 import luau from "@roblox-ts/luau-ast";
 
-export function convertToIndexableExpression(expression: luau.Expression) {
+// overloads prevent unnecessary calls to convertToIndexableExpression
+// where the input expression is already an IndexableExpression
+export function convertToIndexableExpression(expression: luau.IndexableExpression): void;
+export function convertToIndexableExpression(expression: luau.Expression): luau.IndexableExpression;
+export function convertToIndexableExpression(expression: luau.Expression): luau.IndexableExpression {
 	if (luau.isIndexableExpression(expression)) {
 		return expression;
 	}
