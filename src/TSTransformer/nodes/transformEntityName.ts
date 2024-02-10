30da225e99ad87b36a57aeada3e06564d285fdfa
osyrisrblx <osyrisrblx@gmail.com>
2024-02-09 23:13:23 -0500

    Add validateIdentifier to transformEntityName

--- src/TSTransformer/nodes/transformEntityName.ts
+++ src/TSTransformer/nodes/transformEntityName.ts
@@ -1,10 +1,16 @@
 import luau from "@roblox-ts/luau-ast";
 import { TransformState } from "TSTransformer/classes/TransformState";
 import { transformIdentifierDefined } from "TSTransformer/nodes/expressions/transformIdentifier";
+import { validateIdentifier } from "TSTransformer/util/validateIdentifier";
 import ts from "typescript";
 
 export function transformEntityName(state: TransformState, node: ts.EntityName) {
-	return ts.isIdentifier(node) ? transformIdentifierDefined(state, node) : transformQualifiedName(state, node);
+	if (ts.isIdentifier(node)) {
+		validateIdentifier(state, node);
+		return transformIdentifierDefined(state, node);
+	} else {
+		return transformQualifiedName(state, node);
+	}
 }
 
 function transformQualifiedName(state: TransformState, node: ts.QualifiedName): luau.PropertyAccessExpression {
