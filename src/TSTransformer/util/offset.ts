c1bd983a04a234bd76673748a8d49579aa751dcb
Sasial <44125644+sasial-dev@users.noreply.github.com>
2024-02-13 13:37:13 +1000

    Improve output when offset is equal to preceding NumberLiteral (#2567)
    Closes #2226.
    
    This is my first contribution to the codebase, so please point me in the
    right direction in terms of code quality.
    
    Based on the
    [repro](https://roblox-ts.com/playground/#code/CYUwxgNghgTiAEYD2A7AzgF3gByUiAXPJjAJYoDmA2gLoBQdy6WFIGASlCsEgLbwBeeAAoAlIIB8OPBCq8oGABYA6GFx69hARgA00-MrSkAXiDHiAtPC30gA),
    roblox-ts does not output well when you are accounting for the indexing
    change in your code.
    
    I wasn't sure where this required tests, as this would need to test
    compilation into luau AST not the final compiled result. I wrote a quick
    test case in a temp folder mocking a model to check if the output
    compiles correctly.
    
    ---------
    
    Co-authored-by: osyrisrblx <osyrisrblx@gmail.com>

--- src/TSTransformer/util/offset.ts
+++ src/TSTransformer/util/offset.ts
@@ -1,5 +1,7 @@
 import luau from "@roblox-ts/luau-ast";
 
+function getLiteralNumberValue(expression: luau.NumberLiteral): number;
+function getLiteralNumberValue(expression: luau.Expression): number | undefined;
 function getLiteralNumberValue(expression: luau.Expression): number | undefined {
 	if (luau.isNumberLiteral(expression)) {
 		return Number(expression.value);
@@ -15,6 +17,27 @@ function getLiteralNumberValue(expression: luau.Expression): number | undefined 
 export function offset(expression: luau.Expression, value: number) {
 	if (value === 0) {
 		return expression;
+	}
+	// this special case handles when the offset function is called next to a binary expression that has a number literal on its right
+	// IE: array[offset - 1 + 1] -> array[offset]
+	if (
+		luau.isBinaryExpression(expression) &&
+		luau.isNumberLiteral(expression.right) &&
+		(expression.operator === "+" || expression.operator === "-")
+	) {
+		value *= expression.operator === "-" ? -1 : 1;
+		const rightValue = getLiteralNumberValue(expression.right);
+		if (rightValue + value === 0) {
+			return expression.left;
+		} else {
+			return luau.create(luau.SyntaxKind.BinaryExpression, {
+				left: expression.left,
+				operator: expression.operator,
+				right: luau.create(luau.SyntaxKind.NumberLiteral, {
+					value: (rightValue + value).toString(),
+				}),
+			});
+		}
 	}
 	const literalValue = getLiteralNumberValue(expression);
 	if (literalValue !== undefined) {
