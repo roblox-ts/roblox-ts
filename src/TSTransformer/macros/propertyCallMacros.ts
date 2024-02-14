cb05e1f679b3d7d82d6e2cdb4147c8c613064384
Osyris <osyrisrblx@gmail.com>
2024-02-13 20:39:56 -0500

    Integer Division (#2606)
    Adds support for:
    - `Number.idiv(this: number, other: number): number`
    - `Vector2.idiv(this: Vector2, other: Vector2 | number): Vector2`
    - `Vector3.idiv(this: Vector3, other: Vector3 | number): Vector3`
    
    Number literals must be wrapped in parentheses to use `.idiv()`, i.e.
    `(123).idiv(456)`.
    
    Depends on https://github.com/roblox-ts/types/pull/1170
    
    Unit tests for `Vector2.idiv` and `Vector3.idiv` are blocked by Lune not
    supporting them. Issue to track this is here:
    https://github.com/roblox-ts/roblox-ts/issues/2607

--- src/TSTransformer/macros/propertyCallMacros.ts
+++ src/TSTransformer/macros/propertyCallMacros.ts
@@ -19,11 +19,12 @@ function makeMathMethod(operator: luau.BinaryOperator): PropertyCallMacro {
 	};
 }
 
-const OPERATOR_TO_NAME_MAP = new Map<luau.BinaryOperator, "add" | "sub" | "mul" | "div">([
+const OPERATOR_TO_NAME_MAP = new Map<luau.BinaryOperator, "add" | "sub" | "mul" | "div" | "idiv">([
 	["+", "add"],
 	["-", "sub"],
 	["*", "mul"],
 	["/", "div"],
+	["//", "idiv"],
 ]);
 
 function makeMathSet(...operators: Array<luau.BinaryOperator>) {
@@ -920,10 +921,11 @@ export const PROPERTY_CALL_MACROS: { [className: string]: MacroList<PropertyCall
 	CFrame: makeMathSet("+", "-", "*"),
 	UDim: makeMathSet("+", "-"),
 	UDim2: makeMathSet("+", "-"),
-	Vector2: makeMathSet("+", "-", "*", "/"),
+	Vector2: makeMathSet("+", "-", "*", "/", "//"),
 	Vector2int16: makeMathSet("+", "-", "*", "/"),
-	Vector3: makeMathSet("+", "-", "*", "/"),
+	Vector3: makeMathSet("+", "-", "*", "/", "//"),
 	Vector3int16: makeMathSet("+", "-", "*", "/"),
+	Number: makeMathSet("//"),
 
 	String: STRING_CALLBACKS,
 	ArrayLike: ARRAY_LIKE_METHODS,
