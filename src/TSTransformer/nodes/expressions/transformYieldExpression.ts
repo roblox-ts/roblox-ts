4e6974d15c78c33d2731391758fd42473b677f4f
xethlyx <46338199+xethlyx@users.noreply.github.com>
2024-04-02 18:02:30 -0400

    Prevent unnecessary variable in yield used as a statement

--- src/TSTransformer/nodes/expressions/transformYieldExpression.ts
+++ src/TSTransformer/nodes/expressions/transformYieldExpression.ts
@@ -2,6 +2,7 @@ import luau from "@roblox-ts/luau-ast";
 import { TransformState } from "TSTransformer/classes/TransformState";
 import { transformExpression } from "TSTransformer/nodes/expressions/transformExpression";
 import { convertToIndexableExpression } from "TSTransformer/util/convertToIndexableExpression";
+import { isUsedAsStatement } from "TSTransformer/util/isUsedAsStatement";
 import ts from "typescript";
 
 export function transformYieldExpression(state: TransformState, node: ts.YieldExpression) {
@@ -12,7 +13,22 @@ export function transformYieldExpression(state: TransformState, node: ts.YieldEx
 	const expression = transformExpression(state, node.expression);
 	if (node.asteriskToken) {
 		const loopId = luau.tempId("result");
-		const returnValue = state.pushToVar(undefined, "returnValue");
+
+		const finalizer = luau.list.make<luau.Statement>(luau.create(luau.SyntaxKind.BreakStatement, {}));
+		let evaluated: luau.Expression = luau.none();
+
+		if (!isUsedAsStatement(node)) {
+			const returnValue = state.pushToVar(undefined, "returnValue");
+			luau.list.unshift(
+				finalizer,
+				luau.create(luau.SyntaxKind.Assignment, {
+					left: returnValue,
+					operator: "=",
+					right: luau.property(loopId, "value"),
+				}),
+			);
+			evaluated = returnValue;
+		}
 
 		state.prereq(
 			luau.create(luau.SyntaxKind.ForStatement, {
@@ -21,14 +37,7 @@ export function transformYieldExpression(state: TransformState, node: ts.YieldEx
 				statements: luau.list.make<luau.Statement>(
 					luau.create(luau.SyntaxKind.IfStatement, {
 						condition: luau.property(loopId, "done"),
-						statements: luau.list.make<luau.Statement>(
-							luau.create(luau.SyntaxKind.Assignment, {
-								left: returnValue,
-								operator: "=",
-								right: luau.property(loopId, "value"),
-							}),
-							luau.create(luau.SyntaxKind.BreakStatement, {}),
-						),
+						statements: finalizer,
 						elseBody: luau.list.make(),
 					}),
 					luau.create(luau.SyntaxKind.CallStatement, {
@@ -38,7 +47,7 @@ export function transformYieldExpression(state: TransformState, node: ts.YieldEx
 			}),
 		);
 
-		return returnValue;
+		return evaluated;
 	} else {
 		return luau.call(luau.globals.coroutine.yield, [expression]);
 	}
