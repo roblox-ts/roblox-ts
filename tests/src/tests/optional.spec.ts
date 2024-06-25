roblox-ts/roblox-ts

commit 20e7edf4d4d313f52e33bd8b91b6aa95f4b99077

Fix noRobloxSymbolInstanceof diagnostic (#2672)

src/TSTransformer/util/types.ts
@@ -1,8 +1,10 @@
+import path from "path";
+import { RBXTS_SCOPE } from "Shared/constants";
+import { isPathDescendantOf } from "Shared/util/isPathDescendantOf";
 import { SYMBOL_NAMES, TransformState } from "TSTransformer";
 import { NOMINAL_LUA_TUPLE_NAME } from "TSTransformer/classes/MacroManager";
 import { isTemplateLiteralType } from "TSTransformer/typeGuards";
 import ts from "typescript";
-
 type TypeCheck = (type: ts.Type) => boolean;
 
 function getRecursiveBaseTypesInner(result: Array<ts.Type>, type: ts.InterfaceType) {
@@ -192,6 +194,15 @@ export function isEmptyStringType(type: ts.Type) {
 		return type.texts.length === 0 || type.texts.every(v => v.length === 0);
 	}
 	return isStringType(type);
+}
+
+export function isRobloxType(state: TransformState): TypeCheck {
+	const typesPath = path.join(state.data.nodeModulesPath, RBXTS_SCOPE, "types");
+	return type =>
+		type.symbol.declarations?.some(d => {
+			const filePath = d.getSourceFile()?.fileName;
+			return filePath !== undefined && isPathDescendantOf(filePath, typesPath);
+		}) ?? false;
 }
 
 // type utilities

src/TSTransformer/nodes/expressions/transformBinaryExpression.ts
@@ -20,14 +20,14 @@ import { createBinaryFromOperator } from "TSTransformer/util/createBinaryFromOpe
 import { ensureTransformOrder } from "TSTransformer/util/ensureTransformOrder";
 import { getAssignableValue } from "TSTransformer/util/getAssignableValue";
 import { getKindName } from "TSTransformer/util/getKindName";
-import { isSymbolFromRobloxTypes } from "TSTransformer/util/isSymbolFromRobloxTypes";
 import { isUsedAsStatement } from "TSTransformer/util/isUsedAsStatement";
 import { skipDownwards } from "TSTransformer/util/traversal";
 import {
-	getFirstDefinedSymbol,
 	isDefinitelyType,
 	isLuaTupleType,
 	isNumberType,
+	isPossiblyType,
+	isRobloxType,
 	isStringType,
 } from "TSTransformer/util/types";
 import { validateNotAnyType } from "TSTransformer/util/validateNotAny";
@@ -226,8 +226,7 @@ export function transformBinaryExpression(state: TransformState, node: ts.Binary
 			luau.nil(),
 		);
 	} else if (operatorKind === ts.SyntaxKind.InstanceOfKeyword) {
-		const symbol = getFirstDefinedSymbol(state, state.getType(node.right));
-		if (isSymbolFromRobloxTypes(state, symbol)) {
+		if (isPossiblyType(state.getType(node.right), isRobloxType(state))) {
 			DiagnosticService.addDiagnostic(errors.noRobloxSymbolInstanceof(node.right));
 		}
 		return luau.call(state.TS(node, "instanceof"), [left, right]);

src/TSTransformer/nodes/expressions/transformCallExpression.ts
@@ -11,8 +11,7 @@ import { convertToIndexableExpression } from "TSTransformer/util/convertToIndexa
 import { ensureTransformOrder } from "TSTransformer/util/ensureTransformOrder";
 import { expressionMightMutate } from "TSTransformer/util/expressionMightMutate";
 import { isMethod } from "TSTransformer/util/isMethod";
-import { isSymbolFromRobloxTypes } from "TSTransformer/util/isSymbolFromRobloxTypes";
-import { getFirstDefinedSymbol, isPossiblyType, isUndefinedType } from "TSTransformer/util/types";
+import { getFirstDefinedSymbol, isPossiblyType, isRobloxType, isUndefinedType } from "TSTransformer/util/types";
 import { validateNotAnyType } from "TSTransformer/util/validateNotAny";
 import { valueToIdStr } from "TSTransformer/util/valueToIdStr";
 import { wrapReturnIfLuaTuple } from "TSTransformer/util/wrapReturnIfLuaTuple";
@@ -90,11 +89,11 @@ function runCallMacro(
  */
 function fixVoidArgumentsForRobloxFunctions(
 	state: TransformState,
-	symbol: ts.Symbol | undefined,
+	type: ts.Type,
 	args: Array<luau.Expression>,
 	nodeArguments: ReadonlyArray<ts.Expression>,
 ) {
-	if (isSymbolFromRobloxTypes(state, symbol)) {
+	if (isPossiblyType(type, isRobloxType(state))) {
 		for (let i = 0; i < args.length; i++) {
 			const arg = args[i];
 			const nodeArg = nodeArguments[i];
@@ -137,7 +136,7 @@ export function transformCallExpressionInner(
 	}
 
 	const [args, prereqs] = state.capture(() => ensureTransformOrder(state, nodeArguments));
-	fixVoidArgumentsForRobloxFunctions(state, symbol, args, nodeArguments);
+	fixVoidArgumentsForRobloxFunctions(state, expType, args, nodeArguments);
 
 	if (!luau.list.isEmpty(prereqs) && expressionMightMutate(state, expression, node.expression)) {
 		expression = state.pushToVar(expression, "fn");
@@ -179,7 +178,7 @@ export function transformPropertyCallExpressionInner(
 	}
 
 	const [args, prereqs] = state.capture(() => ensureTransformOrder(state, nodeArguments));
-	fixVoidArgumentsForRobloxFunctions(state, symbol, args, nodeArguments);
+	fixVoidArgumentsForRobloxFunctions(state, expType, args, nodeArguments);
 
 	if (!luau.list.isEmpty(prereqs) && expressionMightMutate(state, baseExpression, expression.expression)) {
 		baseExpression = state.pushToVar(baseExpression);
@@ -247,7 +246,7 @@ export function transformElementCallExpressionInner(
 		ensureTransformOrder(state, [argumentExpression, ...nodeArguments]),
 	);
 
-	fixVoidArgumentsForRobloxFunctions(state, symbol, args, nodeArguments);
+	fixVoidArgumentsForRobloxFunctions(state, expType, args, nodeArguments);
 
 	if (!luau.list.isEmpty(prereqs) && expressionMightMutate(state, baseExpression, expression.expression)) {
 		baseExpression = state.pushToVar(baseExpression);

src/TSTransformer/util/isSymbolFromRobloxTypes.ts
@@ -1,11 +1,0 @@
-import path from "path";
-import { RBXTS_SCOPE } from "Shared/constants";
-import { isPathDescendantOf } from "Shared/util/isPathDescendantOf";
-import { TransformState } from "TSTransformer/classes/TransformState";
-import ts from "typescript";
-
-export function isSymbolFromRobloxTypes(state: TransformState, symbol: ts.Symbol | undefined) {
-	const filePath = symbol?.valueDeclaration?.getSourceFile()?.fileName;
-	const typesPath = path.join(state.data.nodeModulesPath, RBXTS_SCOPE, "types");
-	return filePath !== undefined && isPathDescendantOf(filePath, typesPath);
-}

tests/src/diagnostics/noRobloxSymbolInstanceof.1.ts
@@ -1,0 +1,1 @@
+({}) instanceof CFrame;

tests/src/diagnostics/noRobloxSymbolInstanceof.2.ts
@@ -1,0 +1,1 @@
+({}) instanceof pairs;

tests/src/diagnostics/noRobloxSymbolInstanceof.3.ts
@@ -1,0 +1,3 @@
+type Helper = 1 extends number ? CFrameConstructor : typeof Map;
+declare const x: Helper;
+({}) instanceof x;

tests/src/diagnostics/noRobloxSymbolInstanceof.4.ts
@@ -1,0 +1,3 @@
+type Helper = CFrameConstructor | typeof Map;
+declare const x: Helper;
+({}) instanceof x;

commit b16adace4d2da0097c40a2f24ca0ffa3350d312f

Fix optional function call crash (#2731)
### Callstack
```
TypeError: Cannot read properties of undefined (reading 'declarations')
    at roblox-ts/out/tstransformer/util/types.js:198:41
    at roblox-ts/out/tstransformer/util/types.js:64:37
    at Array.some (<anonymous>)
    at isPossiblyTypeInner (roblox-ts/out/tstransformer/util/types.js:64:26)
    at roblox-ts/out/tstransformer/util/types.js:49:37
    at Array.some (<anonymous>)
    at isPossiblyTypeInner (roblox-ts/out/tstransformer/util/types.js:49:27)
    at isPossiblyType (roblox-ts/out/tstransformer/util/types.js:69:12)
    at fixVoidArgumentsForRobloxFunctions (roblox-ts/out/tstransformer/nodes/expressions/transformCallExpression.js:67:36)
    at transformCallExpressionInner (roblox-ts/out/tstransformer/nodes/expressions/transformCallExpression.js:100:5)
```

Regression accidentally introduced by #2672

`type.symbol` will not exist in the case of `undefined` type, so reading
`type.symbol.declarations` causes a crash.

Discovered this issue while compiling this line in
slither.(https://github.com/littensy/slither/blob/67ec115a66444a01603919721a52d26b42d6b449/src/client/components/ui/primary-button.tsx#L46)

src/TSTransformer/util/types.ts
@@ -199,7 +199,7 @@ export function isEmptyStringType(type: ts.Type) {
 export function isRobloxType(state: TransformState): TypeCheck {
 	const typesPath = path.join(state.data.nodeModulesPath, RBXTS_SCOPE, "types");
 	return type =>
-		type.symbol.declarations?.some(d => {
+		type.symbol?.declarations?.some(d => {
 			const filePath = d.getSourceFile()?.fileName;
 			return filePath !== undefined && isPathDescendantOf(filePath, typesPath);
 		}) ?? false;

tests/src/tests/optional.spec.ts
@@ -1,0 +1,6 @@
+export = () => {
+	describe("should support optional function calls", () => {
+		const foo = (() => "bar") as (() => void) | undefined;
+		expect(foo?.()).to.equal("bar");
+	});
+};