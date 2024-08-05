9a0b58e45f8e54f727680594ebea94d3a6e3ef23
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-08-05 09:44:30 +0300

    feat: implement set & map destruction

--- src/TSTransformer/util/binding/getAccessorForBindingType.ts
+++ src/TSTransformer/util/binding/getAccessorForBindingType.ts
@@ -80,7 +80,38 @@ const stringAccessor: BindingAccessor = (state, parentId, index, idStack, isOmit
 	}
 };
 
-const setAccessor: BindingAccessor = (state, parentId, index, idStack, isOmitted) => {
+const setAccessor: BindingAccessor = (state, parentId, index, idStack, isOmitted, isSpread) => {
+	if (isSpread) {
+		const extracted = state.pushToVar(luau.set(idStack), "extracted");
+		const rest = state.pushToVar(luau.array(), "rest");
+
+		const keyId = luau.tempId("k");
+		const loop = luau.create(luau.SyntaxKind.ForStatement, {
+			ids: luau.list.make(keyId, luau.tempId("v")),
+			expression: parentId,
+			statements: luau.list.make(
+				luau.create(luau.SyntaxKind.IfStatement, {
+					condition: luau.unary(
+						"not",
+						luau.create(luau.SyntaxKind.ComputedIndexExpression, {
+							expression: extracted,
+							index: keyId,
+						}),
+					),
+					elseBody: luau.list.make(),
+					statements: luau.list.make(
+						luau.create(luau.SyntaxKind.CallStatement, {
+							expression: luau.call(luau.globals.table.insert, [rest, keyId]),
+						}),
+					),
+				}),
+			),
+		});
+
+		state.prereq(loop);
+		return rest;
+	}
+
 	const args = [parentId];
 	const lastId = peek(idStack);
 	if (lastId) {
@@ -102,6 +133,42 @@ const setAccessor: BindingAccessor = (state, parentId, index, idStack, isOmitted
 };
 
 const mapAccessor: BindingAccessor = (state, parentId, index, idStack, isOmitted, isSpread) => {
+	if (isSpread) {
+		const extracted = state.pushToVar(luau.set(idStack), "extracted");
+		const rest = state.pushToVar(luau.array(), "rest");
+
+		const keyId = luau.tempId("k");
+		const valueId = luau.tempId("v");
+		const loop = luau.create(luau.SyntaxKind.ForStatement, {
+			ids: luau.list.make(keyId, valueId),
+			expression: parentId,
+			statements: luau.list.make(
+				luau.create(luau.SyntaxKind.IfStatement, {
+					condition: luau.unary(
+						"not",
+						luau.create(luau.SyntaxKind.ComputedIndexExpression, {
+							expression: extracted,
+							index: keyId,
+						}),
+					),
+					elseBody: luau.list.make(),
+					statements: luau.list.make(
+						luau.create(luau.SyntaxKind.CallStatement, {
+							expression: luau.call(luau.globals.table.insert, [
+								rest,
+								luau.create(luau.SyntaxKind.Array, {
+									members: luau.list.make(keyId, valueId),
+								}),
+							]),
+						}),
+					),
+				}),
+			),
+		});
+
+		state.prereq(loop);
+		return rest;
+	}
 	const args = [parentId];
 	const lastId = peek(idStack);
 	if (lastId) {
