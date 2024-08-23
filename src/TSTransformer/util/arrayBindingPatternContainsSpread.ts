2308931a57ec145bb6ac613da1336ab5a68aa7b3
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-08-23 17:15:02 +0300

    fix: remove outdated comment and parse nested binding patterns

--- src/TSTransformer/util/arrayBindingPatternContainsSpread.ts
+++ src/TSTransformer/util/arrayBindingPatternContainsSpread.ts
@@ -2,9 +2,10 @@ import ts from "typescript";
 
 export function arrayBindingPatternContainsSpread(arrayBindingPattern: ts.ArrayBindingPattern): boolean {
 	for (const element of arrayBindingPattern.elements) {
-		// If it's not a ts.BindingElement, it must be a ts.OmittedExpression. In that case, doesn't create a variable.
-		// For non-identifiers, element.name must be a nested array/object binding pattern.
-		// For those cases, the hoisting logic is handled elsewhere and the variable here will be a tempId.
+		if (ts.isArrayBindingPattern(element)) {
+			if (arrayBindingPatternContainsSpread(element)) return true;
+		}
+
 		if (ts.isBindingElement(element) && ts.isIdentifier(element.name)) {
 			if (element.dotDotDotToken) return true;
 		}
