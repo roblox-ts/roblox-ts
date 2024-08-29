96dfcd4a4424cb058aeea4955e06e4187294ed80
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-08-29 08:04:25 +0300

    fix: do not necessarily check for identifier

--- src/TSTransformer/util/arrayBindingPatternContainsSpread.ts
+++ src/TSTransformer/util/arrayBindingPatternContainsSpread.ts
@@ -2,12 +2,12 @@ import ts from "typescript";
 
 export function arrayBindingPatternContainsSpread(arrayBindingPattern: ts.ArrayBindingPattern): boolean {
 	for (const element of arrayBindingPattern.elements) {
-		if (ts.isArrayBindingPattern(element)) {
-			if (arrayBindingPatternContainsSpread(element)) return true;
+		if (ts.isBindingElement(element)) {
+			if (element.dotDotDotToken) return true;
 		}
 
-		if (ts.isBindingElement(element) && ts.isIdentifier(element.name)) {
-			if (element.dotDotDotToken) return true;
+		if (ts.isArrayBindingPattern(element)) {
+			if (arrayBindingPatternContainsSpread(element)) return true;
 		}
 	}
 	return false;
