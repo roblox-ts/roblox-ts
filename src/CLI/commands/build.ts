64dcf05381eb89b1cf1dfe93647fcb6b5d5be8aa
osyrisrblx <osyrisrblx@gmail.com>
2024-09-01 17:03:53 -0400

    Fix ESLint issues

--- src/CLI/commands/build.ts
+++ src/CLI/commands/build.ts
@@ -46,7 +46,7 @@ interface BuildFlags {
 /**
  * Defines the behavior for the `rbxtsc build` command.
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export = ts.identity<yargs.CommandModule<{}, BuildFlags & Partial<ProjectOptions>>>({
 	command: ["$0", "build"],
 
