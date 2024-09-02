35890033b6c0231cc016bc4de5e1db45cb9558f4
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2024-09-02 03:14:56 +0200

    Add back getFlags.ts

---
+++ src/TSTransformer/util/getFlags.ts
@@ -0,0 +1,11 @@
+// For debugging purposes: Extracts flag names from a bitfield
+
+export function getFlags<T extends number>(flags: T, from: Record<string | number, string | number>) {
+	const results = new Array<string>();
+	for (const [flagName, flagValue] of Object.entries(from)) {
+		if (typeof flagValue === "number" && !!(flags & flagValue)) {
+			results.push(flagName);
+		}
+	}
+	return results;
+}
