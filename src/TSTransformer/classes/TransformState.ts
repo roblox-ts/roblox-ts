roblox-ts/roblox-ts

commit 23f8a4ae6f58c21e89fbdc5ea206da03f0440710

Fix importing directories that are symlinked twice (#2704)
When walking `getSymlinkedDirectories`, we were overwriting a file in
the `reverseSymlinkMap` if it was reachable through two different
symlink paths. This PR switches to TypeScript's
`getSymlinkedDirectories`, which is similar to `reverseSymlinkMap` but
with multiple possible entries as the value.

Co-authored-by: Osyris <osyrisrblx@gmail.com>

src/TSTransformer/classes/TransformState.ts
@@ -51,7 +51,6 @@ export class TransformState {
 		public readonly rojoResolver: RojoResolver,
 		public readonly pkgRojoResolvers: Array<RojoResolver>,
 		public readonly nodeModulesPathMapping: Map<string, string>,
-		public readonly reverseSymlinkMap: Map<string, string>,
 		public readonly runtimeLibRbxPath: RbxPath | undefined,
 		public readonly typeChecker: ts.TypeChecker,
 		public readonly projectType: ProjectType,
@@ -365,6 +364,9 @@ export class TransformState {
 
 	/** attempts to reverse symlink lookup */
 	public guessVirtualPath(fsPath: string) {
+		const reverseSymlinkMap = this.program.getSymlinkCache?.().getSymlinkedDirectoriesByRealpath();
+		if (!reverseSymlinkMap) return;
+
 		const original = fsPath;
 		while (true) {
 			// reverseSymlinkMap always has trailing slashes
@@ -372,12 +374,11 @@ export class TransformState {
 			const parent = ts.ensureTrailingDirectorySeparator(path.dirname(fsPath));
 			if (fsPath === parent) break;
 			fsPath = parent;
-			const symlink = this.reverseSymlinkMap.get(fsPath);
+			const symlink = reverseSymlinkMap.get(fsPath as ts.Path)?.[0];
 			if (symlink) {
 				return path.join(symlink, path.relative(fsPath, original));
 			}
 		}
-		return original;
 	}
 
 	public symbolToIdMap = new Map<ts.Symbol, luau.TemporaryIdentifier>();

src/Project/classes/VirtualProject.ts
@@ -135,7 +135,6 @@ export class VirtualProject {
 			this.rojoResolver,
 			this.pkgRojoResolvers,
 			this.nodeModulesPathMapping,
-			new Map(),
 			runtimeLibRbxPath,
 			this.typeChecker,
 			projectType,

src/Project/functions/compileFiles.ts
@@ -41,21 +41,6 @@ function emitResultFailure(messageText: string): ts.EmitResult {
 	};
 }
 
-function getReverseSymlinkMap(program: ts.Program) {
-	const result = new Map<string, string>();
-
-	const directoriesMap = program.getSymlinkCache?.()?.getSymlinkedDirectories();
-	if (directoriesMap) {
-		directoriesMap.forEach((dir, fsPath) => {
-			if (typeof dir !== "boolean") {
-				result.set(dir.real, fsPath);
-			}
-		});
-	}
-
-	return result;
-}
-
 /**
  * 'transpiles' TypeScript project into a logically identical Luau project.
  *
@@ -91,8 +76,6 @@ export function compileFiles(
 
 	const pkgRojoResolvers = compilerOptions.typeRoots!.map(RojoResolver.synthetic);
 	const nodeModulesPathMapping = createNodeModulesPathMapping(compilerOptions.typeRoots!);
-
-	const reverseSymlinkMap = getReverseSymlinkMap(program);
 
 	const projectType = data.projectOptions.type ?? inferProjectType(data, rojoResolver);
 
@@ -184,7 +167,6 @@ export function compileFiles(
 				rojoResolver,
 				pkgRojoResolvers,
 				nodeModulesPathMapping,
-				reverseSymlinkMap,
 				runtimeLibRbxPath,
 				typeChecker,
 				projectType,

src/TSTransformer/util/createImportExpression.ts
@@ -188,7 +188,7 @@ export function getImportParts(state: TransformState, sourceFile: ts.SourceFile,
 		return [luau.none()];
 	}
 
-	const virtualPath = state.guessVirtualPath(moduleFile.fileName);
+	const virtualPath = state.guessVirtualPath(moduleFile.fileName) || moduleFile.fileName;
 
 	if (ts.isInsideNodeModules(virtualPath)) {
 		const moduleOutPath = state.pathTranslator.getImportPath(

commit 2ec1e7a8734f31cca7d880564f13ef6d27c62204

Use ts.toPath for guessVirtualPath (#2790)
Fixes a regression from #2704 - ts.toPath normalises the path. Without
it, the path was only found correctly on Unix-like filesystems, since
only they use `/` by default.

src/TSTransformer/classes/TransformState.ts
@@ -6,6 +6,7 @@ import { PARENT_FIELD, ProjectType } from "Shared/constants";
 import { errors, warnings } from "Shared/diagnostics";
 import { ProjectData } from "Shared/types";
 import { assert } from "Shared/util/assert";
+import { getCanonicalFileName } from "Shared/util/getCanonicalFileName";
 import { getOrSetDefault } from "Shared/util/getOrSetDefault";
 import { MultiTransformState } from "TSTransformer";
 import { DiagnosticService } from "TSTransformer/classes/DiagnosticService";
@@ -374,7 +375,9 @@ export class TransformState {
 			const parent = ts.ensureTrailingDirectorySeparator(path.dirname(fsPath));
 			if (fsPath === parent) break;
 			fsPath = parent;
-			const symlink = reverseSymlinkMap.get(fsPath as ts.Path)?.[0];
+			const symlink = reverseSymlinkMap.get(
+				ts.toPath(fsPath, this.program.getCurrentDirectory(), getCanonicalFileName),
+			)?.[0];
 			if (symlink) {
 				return path.join(symlink, path.relative(fsPath, original));
 			}