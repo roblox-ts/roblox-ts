31a49d5c9003054c121ffeaba95f49f231457b49
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-09-08 23:08:03 +0300

    fix: revert spreadDestructArray optimizations

--- src/TSTransformer/util/spreadDestruction/spreadDestructArray.ts
+++ src/TSTransformer/util/spreadDestruction/spreadDestructArray.ts
@@ -2,10 +2,18 @@ import luau from "@roblox-ts/luau-ast";
 import { TransformState } from "TSTransformer/classes/TransformState";
 
 export function spreadDestructArray(state: TransformState, parentId: luau.AnyIdentifier, index: number) {
-	return luau.call(luau.globals.table.move, [
-		parentId,
-		luau.number(index + 1),
-		luau.unary("#", parentId),
-		luau.number(1),
-	]);
+	const restId = state.pushToVar(luau.array(), "rest");
+	state.prereq(
+		luau.create(luau.SyntaxKind.CallStatement, {
+			expression: luau.call(luau.globals.table.move, [
+				parentId,
+				luau.number(index + 1),
+				luau.unary("#", parentId),
+				luau.number(1),
+				restId,
+			]),
+		}),
+	);
+
+	return restId;
 }
