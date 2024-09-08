59b326e8fd84260bdd47ebe3e9f4c8453b566adf
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-09-08 23:02:57 +0300

    fix: combine the nested ifs

--- src/TSTransformer/util/arrayBindingPatternContainsSpread.ts
+++ src/TSTransformer/util/arrayBindingPatternContainsSpread.ts
@@ -2,9 +2,7 @@ import ts from "typescript";
 
 export function arrayBindingPatternContainsSpread(arrayBindingPattern: ts.ArrayBindingPattern): boolean {
 	for (const element of arrayBindingPattern.elements) {
-		if (ts.isBindingElement(element)) {
-			if (element.dotDotDotToken) return true;
-		}
+		if (ts.isBindingElement(element) && element.dotDotDotToken) return true;
 
 		if (ts.isArrayBindingPattern(element)) {
 			if (arrayBindingPatternContainsSpread(element)) return true;
