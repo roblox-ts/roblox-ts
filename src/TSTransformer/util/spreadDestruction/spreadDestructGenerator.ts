e953d05a7f7bf4f7419027fb143d9c3f82fbfeab
wad4444 <77159113+wad4444@users.noreply.github.com>
2024-10-15 00:38:25 +0300

    fix: emit a while loop and check for value.done

--- src/TSTransformer/util/spreadDestruction/spreadDestructGenerator.ts
+++ src/TSTransformer/util/spreadDestruction/spreadDestructGenerator.ts
@@ -2,19 +2,32 @@ import luau from "@roblox-ts/luau-ast";
 import { TransformState } from "TSTransformer/classes/TransformState";
 
 export function spreadDestructGenerator(state: TransformState, parentId: luau.AnyIdentifier) {
-	const keyId = luau.tempId("i");
-	const valueId = luau.tempId("v");
-
 	const restId = state.pushToVar(luau.array(), "rest");
+
+	const valueId = luau.tempId("v");
+	const variable = luau.create(luau.SyntaxKind.VariableDeclaration, {
+		left: valueId,
+		right: luau.call(luau.property(parentId, "next")),
+	});
+
+	const doneCheck = luau.create(luau.SyntaxKind.IfStatement, {
+		condition: luau.create(luau.SyntaxKind.BinaryExpression, {
+			left: luau.property(valueId, "done"),
+			operator: "==",
+			right: luau.create(luau.SyntaxKind.TrueLiteral, {}),
+		}),
+		elseBody: luau.list.make(),
+		statements: luau.list.make(luau.create(luau.SyntaxKind.BreakStatement, {})),
+	});
+
+	const pushToRest = luau.create(luau.SyntaxKind.CallStatement, {
+		expression: luau.call(luau.globals.table.insert, [restId, luau.property(valueId, "value")]),
+	});
+
 	state.prereq(
-		luau.create(luau.SyntaxKind.ForStatement, {
-			expression: luau.property(parentId, "next"),
-			ids: luau.list.make(keyId, valueId),
-			statements: luau.list.make(
-				luau.create(luau.SyntaxKind.CallStatement, {
-					expression: luau.call(luau.globals.table.insert, [restId, valueId]),
-				}),
-			),
+		luau.create(luau.SyntaxKind.WhileStatement, {
+			condition: luau.create(luau.SyntaxKind.TrueLiteral, {}),
+			statements: luau.list.make<luau.Statement>(variable, doneCheck, pushToRest),
 		}),
 	);
 
