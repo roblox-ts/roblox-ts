c9d449982083a26c340a5cbd33f1e457ea2073a0
rimuy <kevinntavares@gmail.com>
2025-06-29 14:58:46 -0300

    create a variable for string indexing if not a number literal

--- src/TSTransformer/nodes/expressions/transformElementAccessExpression.ts
+++ src/TSTransformer/nodes/expressions/transformElementAccessExpression.ts
@@ -51,8 +51,9 @@ export function transformElementAccessExpressionInner(
 	// String indexing
 	if (isDefinitelyType(expType, isStringType)) {
 		const at = offset(index, 1);
+		const indexExp = luau.isNumberLiteral(index) ? at : state.pushToVar(at, "index");
 
-		return luau.call(luau.globals.string.sub, [expression, at, at]);
+		return luau.call(luau.globals.string.sub, [expression, indexExp, indexExp]);
 	}
 
 	if (ts.isDeleteExpression(skipUpwards(node).parent)) {
